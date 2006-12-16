7d9761d8ad81ad7445fef083b7dc99d7b405e14b
wyrmchild <???>
2006-12-16 17:42:04 +0000

    - Server executable is now dpserver instead of dpsrv - Outputted executable is now optimized according to what is defined in Optimize.cmake (Pentium2 with -O2 by default)

---
+++ Optimize.cmake
@@ -0,0 +1,24 @@
+
+add_definitions ( -O2 )
+
+# Intel 386
+# Nothing
+
+#add_definitions ( -march=i386 )
+
+# Intel 686
+# MMX
+
+#add_definitions ( -march=i686 )
+
+# Pentium2.. works for AthlonXP and newer Pentiums
+# Has support for MMX and SSE
+
+add_definitions ( -march=pentium2 )
+
+# AthlonXP
+# SSE, MMX, 3DNow!, Ext 3DNow!
+
+#add_definitions ( -march=athlon-xp )
+
+
