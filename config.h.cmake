49e84486595274bffc36bbec1f098096789a9752
wyrmchild <???>
2006-12-21 16:49:35 +0000

    | Moved config.h updating back to trunk + Added HAVE_BOOST in config.h.cmake

--- config.h.cmake
+++ config.h.cmake
@@ -2,15 +2,15 @@
 
 #cmakedefine IS_BIG_ENDIAN
 
+#cmakedefine HAVE_BOOST
+
 #cmakedefine EV_SELECT_COPY
 #cmakedefine EV_SELECT
 #cmakedefine EV_PSELECT
 #cmakedefine EV_EPOLL
 #cmakedefine EV_KQUEUE
 #cmakedefine EV_KEVENT
-
 #cmakedefine HAVE_SELECT_H
-
 #cmakedefine HAVE_WSA
 
 #cmakedefine NDEBUG
