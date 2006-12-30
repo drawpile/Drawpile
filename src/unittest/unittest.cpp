0b3af262810805b1507bf871c1374684c9fc647d
wyrmchild <???>
2006-12-30 09:08:39 +0000

    adjusted license format

--- src/unittest/unittest.cpp
+++ src/unittest/unittest.cpp
@@ -1,13 +1,12 @@
 /*******************************************************************************
 
    Copyright (C) 2006 M.K.A. <wyrmchild@sourceforge.net>
-   For more info, see: http://drawpile.sourceforge.net/
-
+   
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation; either version 2, or (at your option)
    any later version.
-
+   
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -16,6 +15,10 @@
    You should have received a copy of the GNU General Public License
    along with this program; if not, write to the Free Software Foundation,
    Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+   
+---
+   
+   For more info, see: http://drawpile.sourceforge.net/
 
 *******************************************************************************/
 
