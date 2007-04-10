fb4e50e034d63d61d5eb1bf4d2df38a78dc2e20c
Calle Laakkonen <calle@luolamies.org>
2007-04-10 11:35:15 +0000

    only pop up color dialog on left click. Added missing copyright notice

--- src/client/dualcolorbutton.cpp
+++ src/client/dualcolorbutton.cpp
@@ -1,3 +1,23 @@
+/*
+   DrawPile - a collaborative drawing program.
+
+   Copyright (C) 2006-2007 Calle Laakkonen
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software Foundation,
+   Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+*/
+
 #include <QApplication>
 #include <QPainter>
 #include <QMouseEvent>
@@ -165,6 +185,8 @@ void DualColorButton::mouseMoveEvent(QMouseEvent *event)
 
 void DualColorButton::mouseReleaseEvent(QMouseEvent *event)
 {
+	if(event->button() != Qt::LeftButton)
+		return;
 	const QRect swaprect(qRound(width()*2.0/3.0),0,width()/3,height()/3);
 	if(resetBlackRect().contains(event->pos()) || resetWhiteRect().contains(event->pos())) {
 		foreground_ = Qt::black;
