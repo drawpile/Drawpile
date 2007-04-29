6128c34af6f70e95985dadd28ef3f23fccdb2152
wyrmchild <???>
2007-04-29 00:18:25 +0000

    corrected (I blame blind c&p) and shortened the discplaimer

--- src/shared/datatypes.h
+++ src/shared/datatypes.h
@@ -1,20 +1,6 @@
 /*******************************************************************************
 
-   Copyright (C) 2006, 2007 M.K.A. <wyrmchild@users.sourceforge.net>
-
-   Permission is hereby granted, free of charge, to any person obtaining a
-   copy of this software and associated documentation files (the "Software"),
-   to deal in the Software without restriction, including without limitation
-   the rights to use, copy, modify, merge, publish, distribute, sublicense,
-   and/or sell copies of the Software, and to permit persons to whom the
-   Software is furnished to do so, subject to the following conditions:
-
-   Except as contained in this notice, the name(s) of the above copyright
-   holders shall not be used in advertising or otherwise to promote the sale,
-   use or other dealings in this Software without prior written authorization.
-   
-   The above copyright notice and this permission notice shall be included in
-   all copies or substantial portions of the Software.
+   Copyright (C) 2007 M.K.A. <wyrmchild@users.sourceforge.net>
 
    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
