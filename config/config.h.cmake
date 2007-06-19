5f4110ac85d4ebaaaee35a0cfd3e83039a799fd6
wyrmchild <???>
2007-06-19 11:05:18 +0000

    Allow enabling assembly.

--- config/config.h.cmake
+++ config/config.h.cmake
@@ -3,10 +3,7 @@
 #cmakedefine HAVE_BOOST
 #cmakedefine HAVE_SNPRINTF
 
+#cmakedefine USE_ASM
+
 #cmakedefine HAVE_MMX
 #cmakedefine HAVE_SSE
-#cmakedefine __MMX__
-#cmakedefine __SSE__
-#cmakedefine __SSE2__
-#cmakedefine __SSE3__
-#cmakedefine __SSE4__
