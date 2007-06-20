2f490b12487fd334272fe1e09960e1c605c06136
wyrmchild <???>
2007-06-20 16:51:42 +0000

    Converted first test to use binary digest.

--- src/unittest/SHA1/SHA1_01_bin.cpp
+++ src/unittest/SHA1/SHA1_01_bin.cpp
@@ -23,18 +23,24 @@ int main()
 	
 	unsigned char src1[] = "abc";
 	
-	hash.Update(src1, strlen(reinterpret_cast<char*>(src1)));
+	hash.Update(src1, 3);
 	hash.Final();
 	
-	char hexdigest[41];
-	hash.HexDigest(hexdigest);
-	hexdigest[40] = '\0';
+	unsigned char digest[20];
+	hash.GetHash(digest);
 	
+	unsigned char res[] = {0xA9,0x99,0x3E,0x36,0x47,0x06,0x81,0x6A,0xBA,0x3E,0x25,0x71,0x78,0x50,0xC2,0x6C,0x9C,0xD0,0xD8,0x9D};
 	char res1[] = "A9993E364706816ABA3E25717850C26C9CD0D89D";
 	
-	int rv = memcmp(hexdigest, res1, 40);
+	int rv = memcmp(digest, res, 20);
 	if (rv != 0)
-		std::cerr << "result  : " << hexdigest << std::endl << "expected: " << res1 << std::endl;
+	{
+		char hexdigest[41];
+		hash.HexDigest(hexdigest);
+		hexdigest[40] = 0;
+		std::cerr << "result  : " << hexdigest << std::endl
+			<< "expected: " << res1 << std::endl;
+	}
 	
 	return (rv == 0 ? EXIT_SUCCESS : EXIT_FAILURE);
 }
