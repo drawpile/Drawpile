976a7b8aa33c5d5ceceb53b62b02801833f7f5a6
wyrmchild <???>
2007-07-28 17:40:54 +0000

    Removed some optimizations that were automatically enabled at certain -O levels. Added other optimizations which should be profiled properly later for their usefulness. Added more profiling options. Restored Pentium2 as default architecture.

--- config/Optimize.cmake
+++ config/Optimize.cmake
@@ -1,10 +1,10 @@
 # Optimize.cmake
 
 # http://gcc.gnu.org/onlinedocs/gcc/i386-and-x86_002d64-Options.html
-set ( CPU pentium3 )
+set ( CPU pentium2 )
 
 if ( GENERIC )
-	set ( CPU pentium3 ) # our target audience likely don't have older
+	set ( CPU pentium2 ) # our target audience likely don't have older
 endif ( GENERIC )
 
 # 0 - no optimization
@@ -25,20 +25,22 @@ set ( MTUNE_GENERIC "-mtune=generic") # GCC >=4.x
 
 set ( FASTMATH "-ffast-math" )
 
+set ( PROFILING_FLAGS2 "-finstrument-functions" )
 set ( PROFILING_FLAGS "-pg" )
 set ( DEBUG_FLAGS "-g -Wall" )
 set ( OPT "-O${OPTIMIZATION}" )
 
-set ( FOMIT "-fomit-frame-pointer" )
-
 set ( WARNALL "-Wall" )
 
 set ( NORTTI "-fno-rtti" )
+set ( GCSELAS "-fgcse-las" )
+set ( GCSELAS2 "-fgcse-after-reload" )
+
+set ( PROFARCS "-fprofile-arcs" )
+set ( PROFVPT "-fvpt" )
 
 # a bit questionable optimizations
 set ( U_MFPMATH "-mfpmath=sse,387" ) # does not break anything, only requires SSE to be present
-set ( U_MALIGN "-malign-double" ) # breaks binary compatibility?
-set ( U_SCHED "-fschedule-insns" )
 set ( U_NOTRAP "-fno-trapping-math" )
 set ( U_UNSAFE "-funsafe-math-optimizations" )
 
@@ -82,25 +84,26 @@ if ( NOT FAST_MATH )
 	set ( FASTMATH "" )
 endif ( NOT FAST_MATH )
 
-###   TEST -fomit-frame-pointer   ###
-
-if ( NOT PROFILE )
-	check_cxx_accepts_flag ( ${FOMIT} ACCEPT_FOMIT )
-endif ( NOT PROFILE )
-
-if ( NOT ACCEPT_FOMIT )
-	set ( FOMIT "" )
-endif ( NOT ACCEPT_FOMIT )
-
-###   TEST -pg   ###
+###   TEST -pg   and other profiling flags  ###
 
 if ( PROFILE )
 	check_cxx_accepts_flag ( ${PROFILING_FLAGS} ACCEPT_PROFILE )
+	check_cxx_accepts_flag ( ${PROFILING_FLAGS2} ACCEPT_PROFILE2 )
+	check_cxx_accepts_flag ( ${PROFARCS} ACCEPT_PROFARCS )
 endif ( PROFILE )
+
 
 if ( NOT ACCEPT_PROFILE )
 	set ( PROFILING_FLAGS "" )
 endif ( NOT ACCEPT_PROFILE )
+if ( NOT ACCEPT_PROFILE2 )
+	set ( PROFILING_FLAGS2 "" )
+endif ( NOT ACCEPT_PROFILE2 )
+if ( NOT ACCEPT_PROFARCS )
+	set ( PROFARCS "" )
+endif ( NOT ACCEPT_PROFARCS )
+
+set ( ALL_PROFILING_FLAGS ${PROFILING_FLAGS} ${PROFILING_FLAGS2} ${PROFARCS} )
 
 ###   TEST -pipe   ###
 
@@ -111,9 +114,8 @@ endif ( NOT ACCEPT_PIPE )
 
 ###   TEST unsafe math optimizations   ###
 
-if ( UNSAFE_MATH )
+if ( UNSAFEOPT )
 	check_cxx_accepts_flag ( ${U_MFPMATH} ACCEPT_MFPMATH )
-	check_cxx_accepts_flag ( ${U_MALIGN} ACCEPT_MALIGN )
 	check_cxx_accepts_flag ( ${U_SCHED} ACCEPT_SCHED )
 	check_cxx_accepts_flag ( ${U_NOTRAP} ACCEPT_NOTRAP )
 	check_cxx_accepts_flag ( ${U_UNSAFE} ACCEPT_UNSAFE )
@@ -121,9 +123,6 @@ if ( UNSAFE_MATH )
 	if ( NOT ACCEPT_MFPMATH )
 		set ( U_MFPMATH "" )
 	endif ( NOT ACCEPT_MFPMATH )
-	if ( NOT ACCEPT_MALIGN )
-		set ( U_MALIGN "" )
-	endif ( NOT ACCEPT_MALIGN )
 	if ( NOT ACCEPT_SCHED )
 		set ( U_SCHED "" )
 	endif ( NOT ACCEPT_SCHED )
@@ -134,10 +133,10 @@ if ( UNSAFE_MATH )
 		set ( U_UNSAFE "" )
 	endif ( NOT ACCEPT_UNSAFE )
 	
-	set ( UNSAFE_MATH_OPT "${U_MFPMATH} ${U_MALIGN} ${U_SCHED} ${U_NOTRAP} ${U_UNSAFE}")
-else ( UNSAFE_MATH )
+	set ( UNSAFE_MATH_OPT "${U_MFPMATH} ${U_SCHED} ${U_NOTRAP} ${U_UNSAFE}")
+else ( UNSAFEOPT )
 	set ( UNSAFE_MATH_OPT "" )
-endif ( UNSAFE_MATH )
+endif ( UNSAFEOPT )
 
 ###   TEST PIE and PIC   ###
 
@@ -159,6 +158,20 @@ if ( NOPI )
 	set ( NOPI_FLAGS "${NOPIC} ${NOPIE}" )
 endif ( NOPI )
 
+###   TEST STUFF   ###
+
+check_cxx_accepts_flag ( ${GCSELAS} ACCEPT_GCSELAS )
+if ( NOT ACCEPT_GCSELAS )
+	set ( GCSELAS "" )
+endif ( NOT ACCEPT_GCSELAS )
+
+check_cxx_accepts_flag ( ${GCSELAS2} ACCEPT_GCSELAS2 )
+if ( NOT ACCEPT_GCSELAS2 )
+	set ( GCSELAS2 "" )
+endif ( NOT ACCEPT_GCSELAS2 )
+
+set ( MISCOPTS "${GCSELAS} ${GCSELAS2}" )
+
 ###   TEST -fno-rtti   ###
 
 check_cxx_accepts_flag ( ${NORTTI} ACCEPT_NORTTI )
@@ -175,6 +188,6 @@ endif ( NOT ACCEPT_WALL )
 
 ###   Set flags   ###
 
-set ( CMAKE_CXX_FLAGS "${WARNALL} ${PIPE} ${ARCH} ${MTUNE} ${OPT} ${FASTMATH} ${PROFILING_FLAGS} ${UNSAFE_MATH_OPT} ${REGPARM} ${NOPI_FLAGS}" )
+set ( CMAKE_CXX_FLAGS "${WARNALL} ${PIPE} ${ARCH} ${MTUNE} ${OPT} ${MISCOPTS} ${FASTMATH} ${ALL_PROFILING_FLAGS} ${UNSAFE_MATH_OPT} ${REGPARM} ${NOPI_FLAGS}" )
 set ( CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS} ${DEBUG_FLAGS}" )
-set ( CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS} ${FOMIT} -DNDEBUG" )
+set ( CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS} -DNDEBUG" )
