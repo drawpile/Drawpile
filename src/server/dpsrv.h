386b31e83013fc6d1eb12fd2f80d02a676054bae
wyrmchild <???>
2007-07-30 23:48:56 +0000

    Added missing files

---
+++ src/server/dpsrv.h
@@ -0,0 +1,34 @@
+/*******************************************************************************
+
+   Copyright (C) 2006, 2007 M.K.A. <wyrmchild@users.sourceforge.net>
+
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+   DEALINGS IN THE SOFTWARE.
+
+   ---
+
+   For more info, see: http://drawpile.sourceforge.net/
+
+*******************************************************************************/
+
+#ifndef DrawpileServer_INCLUDED
+#define DrawpileServer_INCLUDED
+
+#include <stdexcept> // std::bad_alloc
+
+class Server;
+
+/**
+ * @throw std::bad_alloc
+ */
+static
+void getArgs(int argc, char** argv, Server& srv);
+
+int main(int argc, char** argv);
+
+#endif // DrawpileServer_INCLUDED
