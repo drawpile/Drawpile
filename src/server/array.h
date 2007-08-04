5f630a786daea4713da5091ece2d0c94af4deb5f
wyrmchild <???>
2007-08-04 10:59:30 +0000

    Restored nothrow attribute

--- src/server/array.h
+++ src/server/array.h
@@ -26,45 +26,28 @@ template <typename T>
 struct Array
 {
 	//! Constructor
-	Array() __attribute__ ((nothrow))
-	{
-	}
+	Array() __attribute__ ((nothrow)) __attribute__((nothrow));
 	
 	//! Copy constructor
 	/**
 	 * @param[in] array Source array
 	 */
-	Array(const Array<T>& array)
-		: ptr(array.ptr), size(array.size)
-	{
-	}
+	Array(const Array<T>& array) __attribute__((nothrow));
 	
 	//! Constructor
 	/**
 	 * @param[in] _data char* string to associate with this Array
 	 * @param[in] _size Size of _data
 	 */
-	Array(T* _data, const size_t _size)
-		: ptr(_data), size(_size)
-	{
-		assert((_data == 0 and _size == 0) or (_data != 0 and _size > 0));
-	}
+	Array(T* _data, const size_t _size) __attribute__((nothrow));
 	
 	//! Destructor
 	/**
 	 * @note Deletes contained char* string to avoid memory leak
 	 */
-	~Array()
-	{
-		delete [] ptr;
-	}
+	~Array() __attribute__((nothrow));
 	
-	void set(T* _data, const size_t _size)
-	{
-		delete [] ptr;
-		ptr = _data;
-		size = _size;
-	}
+	void set(T* _data, const size_t _size) __attribute__((nothrow));
 	
 	//! Type of array
 	typedef T type;
@@ -74,5 +57,37 @@ struct Array
 	//! Number of elements in data
 	size_t size;
 };
+
+template <typename T>
+Array<T>::Array()
+{
+}
+
+template <typename T>
+Array<T>::Array(const Array<T>& array)
+	: ptr(array.ptr), size(array.size)
+{
+}
+
+template <typename T>
+Array<T>::Array(T* _data, const size_t _size)
+	: ptr(_data), size(_size)
+{
+	assert((_data == 0 and _size == 0) or (_data != 0 and _size > 0));
+}
+
+template <typename T>
+Array<T>::~Array()
+{
+	delete [] ptr;
+}
+
+template <typename T>
+void Array<T>::set(T* _data, const size_t _size)
+{
+	delete [] ptr;
+	ptr = _data;
+	size = _size;
+}
 
 #endif // Container_H_INCLUDED
