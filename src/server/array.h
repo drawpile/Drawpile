3b02246ad1da10a44e9e15ed579f34f55abbbcb5
wyrmchild <???>
2007-08-04 10:53:53 +0000

    Removed array.cpp so newer GCC can compile it correctly.

--- src/server/array.h
+++ src/server/array.h
@@ -25,28 +25,45 @@ template <typename T>
 struct Array
 {
 	//! Constructor
-	Array() __attribute__ ((nothrow));
+	Array() __attribute__ ((nothrow))
+	{
+	}
 	
 	//! Copy constructor
 	/**
 	 * @param[in] array Source array
 	 */
-	Array(const Array<T>& array) __attribute__ ((nothrow));
+	Array(const Array<T>& array)
+		: ptr(array.ptr), size(array.size)
+	{
+	}
 	
 	//! Constructor
 	/**
 	 * @param[in] _data char* string to associate with this Array
 	 * @param[in] _size Size of _data
 	 */
-	Array(T* _data, const size_t _size) __attribute__ ((nothrow));
+	Array(T* _data, const size_t _size)
+		: ptr(_data), size(_size)
+	{
+		assert((_data == 0 and _size == 0) or (_data != 0 and _size > 0));
+	}
 	
 	//! Destructor
 	/**
 	 * @note Deletes contained char* string to avoid memory leak
 	 */
-	~Array() __attribute__ ((nothrow));
+	~Array()
+	{
+		delete [] ptr;
+	}
 	
-	void set(T* _data, const size_t _size) __attribute__ ((nothrow));
+	void set(T* _data, const size_t _size)
+	{
+		delete [] ptr;
+		ptr = _data;
+		size = _size;
+	}
 	
 	//! Type of array
 	typedef T type;
