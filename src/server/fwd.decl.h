079f43f499a95395a14158543c7e9db05036c0c9
wyrmchild <???>
2007-09-22 18:36:56 +0000

    Added forward declaration files for Server and Shared.

---
+++ src/server/fwd.decl.h
@@ -0,0 +1,19 @@
+#ifndef ServerFwdDecl_INCLUDED
+#define ServerFwdDecl_INCLUDED
+
+class Socket;
+class Address;
+
+class Server;
+class User;
+
+class Session;
+class SessionData;
+
+class LayerData;
+
+class Buffer;
+
+struct Statistics;
+
+#endif // ServerFwdDecl_INCLUDED
