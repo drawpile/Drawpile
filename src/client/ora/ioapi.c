352d85deff48e3290e9fdd1bd8308281af152898
Calle Laakkonen <laakkonenc@gmail.com>
2012-12-30 19:36:28 +0200

    Removed use of OF macro in minizip.
    The OF macro causes a build error on Gentoo and is not
    needed anyway on modern compilers. A more correct fix
    would be to remove the bundled minizip and use the one
    provided by the system.

--- src/client/ora/ioapi.c
+++ src/client/ora/ioapi.c
@@ -72,13 +72,13 @@ void fill_zlib_filefunc64_32_def_from_filefunc32(zlib_filefunc64_32_def* p_filef
 
 
 
-static voidpf  ZCALLBACK fopen_file_func OF((voidpf opaque, const char* filename, int mode));
-static uLong   ZCALLBACK fread_file_func OF((voidpf opaque, voidpf stream, void* buf, uLong size));
-static uLong   ZCALLBACK fwrite_file_func OF((voidpf opaque, voidpf stream, const void* buf,uLong size));
-static ZPOS64_T ZCALLBACK ftell64_file_func OF((voidpf opaque, voidpf stream));
-static long    ZCALLBACK fseek64_file_func OF((voidpf opaque, voidpf stream, ZPOS64_T offset, int origin));
-static int     ZCALLBACK fclose_file_func OF((voidpf opaque, voidpf stream));
-static int     ZCALLBACK ferror_file_func OF((voidpf opaque, voidpf stream));
+static voidpf  ZCALLBACK fopen_file_func (voidpf opaque, const char* filename, int mode);
+static uLong   ZCALLBACK fread_file_func (voidpf opaque, voidpf stream, void* buf, uLong size);
+static uLong   ZCALLBACK fwrite_file_func (voidpf opaque, voidpf stream, const void* buf,uLong size);
+static ZPOS64_T ZCALLBACK ftell64_file_func (voidpf opaque, voidpf stream);
+static long    ZCALLBACK fseek64_file_func (voidpf opaque, voidpf stream, ZPOS64_T offset, int origin);
+static int     ZCALLBACK fclose_file_func (voidpf opaque, voidpf stream);
+static int     ZCALLBACK ferror_file_func (voidpf opaque, voidpf stream);
 
 static voidpf ZCALLBACK fopen_file_func (voidpf opaque, const char* filename, int mode)
 {
