edc794c1197d4b7edb9044cc8bee24b63ebc8a60
Calle Laakkonen <laakkonenc@gmail.com>
2015-04-03 16:46:24 +0300

    Mac deployment fixes

--- scripts/make-mac-pkg.sh
+++ scripts/make-mac-pkg.sh
@@ -20,24 +20,9 @@ BINFILE="$(readlink -n Drawpile)"
 rm Drawpile
 mv "$BINFILE" Drawpile
 cd -
+cd bin
 
-"$QTDIR/bin/macdeployqt" bin/Drawpile.app
+"$QTDIR/bin/macdeployqt" Drawpile.app -dmg
 
-# Construct app bundle
-# See: http://stackoverflow.com/questions/96882/how-do-i-create-a-nice-looking-dmg-for-mac-os-x-using-command-line-tools
-
-mkdir dmg
-cp -r bin/Drawpile.app dmg
-
-# TODO for some reason this generates much larger packages compared to ones
-# created manually with disk utility.
-
-hdiutil create -srcfolder dmg -volname "$TITLE" -fs HFS+ \
-	-fsargs "-c c=64,a=16,e=16" -format UDRW -size 128m pack.temp.dmg
-
-# todo: add styling here
-
-hdiutil convert pack.temp.dmg -format UDZO -imagekey zlib-level=9 -o "${TITLE}.dmg"
-rm -f pack.temp.dmg 
-rm -rf dmg
+mv Drawpile.dmg "Drawpile $VERSION.dmg"
 
