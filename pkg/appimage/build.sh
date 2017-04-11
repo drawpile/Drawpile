cb60c52981d68e0ac15d374761b1dafcb6ca875d
Calle Laakkonen <laakkonenc@gmail.com>
2017-04-11 20:22:03 +0300

    Build AppImage Qt from sources
    Fixes unicode error in QJsonDocument. (EPEL's Qt build is broken)
    https://bugzilla.redhat.com/show_bug.cgi?id=1376832

--- pkg/appimage/build.sh
+++ pkg/appimage/build.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -e
 
+if [ $# -ne 1 ]; then
+	echo "Usage: ./build.sh <shell|client|server>"
+	exit 1
+fi
+
 mkdir -p out
 
 cd ../..
@@ -8,6 +13,16 @@ IMAGE=drawpile2
 SRCVOL="$(pwd):/Drawpile:ro"
 OUTVOL="$(pwd)/pkg/appimage/out:/out"
 
-docker run --rm -ti -v "$SRCVOL" -v "$OUTVOL" $IMAGE bash /Drawpile/pkg/appimage/Build-client
-docker run --rm -ti -v "$SRCVOL" -v "$OUTVOL" $IMAGE bash /Drawpile/pkg/appimage/Build-server
+if [ "$1" == "shell" ]; then
+	CMD=""
+elif [ "$1" == "client" ]; then
+	CMD="/Drawpile/pkg/appimage/Build-client"
+elif [ "$1" == "server" ]; then
+	CMD="/Drawpile/pkg/appimage/Build-server"
+else
+	echo "Unknown build target: $1"
+	exit 1
+fi
+
+docker run --rm -ti -v "$SRCVOL" -v "$OUTVOL" $IMAGE bash $CMD
 
