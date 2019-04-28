31df816628cda1fb7ebe6ec722b3508771efcdd5
Calle Laakkonen <laakkonenc@gmail.com>
2019-04-28 19:59:51 +0300

    Add new file download helper class

--- src/shared/util/networkaccess.cpp
+++ src/shared/util/networkaccess.cpp
@@ -1,7 +1,7 @@
 /*
    Drawpile - a collaborative drawing program.
 
-   Copyright (C) 2014-2017 Calle Laakkonen
+   Copyright (C) 2014-2019 Calle Laakkonen
 
    Drawpile is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -20,11 +20,12 @@
 #include "networkaccess.h"
 
 #include <QNetworkReply>
-#include <QMimeDatabase>
 #include <QMutexLocker>
 #include <QHash>
 #include <QThread>
 #include <QDebug>
+#include <QSaveFile>
+#include <QTemporaryFile>
 
 namespace networkaccess {
 
@@ -70,6 +71,138 @@ QNetworkReply *get(const QUrl &url, const QString &expectType)
 		}
 	});
 	return reply;
+}
+
+FileDownload::FileDownload(QObject *parent)
+	: QObject(parent), m_file(nullptr), m_reply(nullptr), m_maxSize(0), m_size(0)
+{
+}
+
+void FileDownload::setTarget(const QString &path)
+{
+	Q_ASSERT(!m_file);
+	m_file = new QSaveFile(path, this);
+}
+
+void FileDownload::setExpectedHash(const QByteArray &hash, QCryptographicHash::Algorithm algorithm)
+{
+	m_expectedHash = hash;
+	m_hash.reset(new QCryptographicHash(algorithm));
+
+#if QT_VERSION >= QT_VERSION_CHECK(5, 12, 0)
+	if(hash.length() > 0 && QCryptographicHash::hashLength(algorithm) != hash.length()) {
+		qWarning("Expected hash length %d not valid for selected algorithm!", hash.length());
+		m_expectedHash = QByteArray();
+		m_hash.reset();
+	}
+#endif
+}
+void FileDownload::start(const QUrl &url)
+{
+	Q_ASSERT(!m_reply);
+
+	if(!m_file)
+		m_file = new QTemporaryFile(this);
+
+	if(!m_file->open(m_file->inherits("QSaveFile") ? QIODevice::WriteOnly : QIODevice::ReadWrite)) {
+		emit finished(m_file->errorString());
+		return;
+	}
+
+	QNetworkRequest req(url);
+
+	m_reply = getInstance()->get(req);
+	m_reply->setParent(this);
+
+	if(!m_expectedType.isEmpty())
+		connect(m_reply, &QNetworkReply::metaDataChanged, this, &FileDownload::onMetaDataChanged);
+
+	connect(m_reply, &QNetworkReply::downloadProgress, this, &FileDownload::progress);
+	connect(m_reply, &QNetworkReply::readyRead, this, &FileDownload::onReadyRead);
+	connect(m_reply, &QNetworkReply::finished, this, &FileDownload::onFinished);
+}
+
+void FileDownload::onMetaDataChanged()
+{
+	QVariant mimetype = m_reply->header(QNetworkRequest::ContentTypeHeader);
+	if(mimetype.isValid()) {
+		if(!mimetype.toString().startsWith(m_expectedType)==false) {
+			m_reply->abort();
+		}
+	}
+}
+
+void FileDownload::onReadyRead()
+{
+	const auto buffer = m_reply->readAll();
+
+	m_size += buffer.length();
+	if(m_maxSize>0 && m_size > m_maxSize) {
+		m_reply->abort();
+		return;
+	}
+
+	m_file->write(buffer);
+	if(!m_hash.isNull())
+		m_hash->addData(buffer);
+}
+
+void FileDownload::onFinished()
+{
+	if(m_reply->error()) {
+		QString errorMessage;
+
+		if(m_reply->error() == QNetworkReply::OperationCanceledError &&
+			!m_reply->header(QNetworkRequest::ContentTypeHeader).toString().startsWith(m_expectedType)) {
+			errorMessage = tr("Unexpected MIME type (%1)").arg(m_reply->header(QNetworkRequest::ContentTypeHeader).toString());
+
+		} else if(m_reply->error() == QNetworkReply::OperationCanceledError &&
+			 m_maxSize > 0 && m_size > m_maxSize) {
+			 errorMessage = tr("Downloaded file is too big");
+
+		} else {
+			errorMessage = m_reply->errorString();
+		}
+
+		if(errorMessage.isEmpty())
+			errorMessage = "Unknown download error";
+
+		emit finished(errorMessage);
+		return;
+
+	}
+	// No error during download. Check that the checksum matches (if given)
+	if(!m_hash.isNull()) {
+		if(m_hash->result() != m_expectedHash) {
+			emit finished(tr("Downloaded file's checksum does not match."));
+			return;
+		}
+	}
+
+	// Checksum OK! Finalize download...
+	auto *saveFile = qobject_cast<QSaveFile*>(m_file);
+	if(saveFile) {
+		if(!saveFile->commit()) {
+			emit finished(saveFile->errorString());
+			return;
+		}
+
+		// Reopen for reading.
+		const QString filename = saveFile->fileName();
+		delete m_file;
+		m_file = new QFile(filename, this);
+		if(!m_file->open(QIODevice::ReadOnly)) {
+			emit finished(m_file->errorString());
+			return;
+		}
+
+	} else {
+		// Not a QSaveFile? Then just rewind back to the beginning to make
+		// this file ready to use
+		m_file->seek(0);
+	}
+
+	emit finished(QString());
 }
 
 }
--- src/shared/util/networkaccess.h
+++ src/shared/util/networkaccess.h
@@ -1,7 +1,7 @@
 /*
    Drawpile - a collaborative drawing program.
 
-   Copyright (C) 2014-2017 Calle Laakkonen
+   Copyright (C) 2014-2019 Calle Laakkonen
 
    Drawpile is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -21,6 +21,10 @@
 #define NETWORKACCESS_H
 
 #include <functional>
+#include <QObject>
+#include <QCryptographicHash>
+#include <QScopedPointer>
+#include <QFileDevice>
 
 class QNetworkAccessManager;
 class QNetworkReply;
@@ -47,6 +51,84 @@ QNetworkAccessManager *getInstance();
  */
 QNetworkReply *get(const QUrl &url, const QString &expectType);
 
+/**
+ * @brief A file download wrapper that emits progress reports
+ */
+class FileDownload : public QObject {
+	Q_OBJECT
+public:
+	explicit FileDownload(QObject *parent=nullptr);
+
+	/**
+	 * Set target file path
+	 *
+	 * This must be called before *start*. If no target file is set,
+	 * a temporary file is used.
+	 *
+	 * @param path
+	 */
+	void setTarget(const QString &path);
+
+	/**
+	 * Set the maximum size of the download
+	 *
+	 * This must be called before *start*
+	 */
+	void setMaxSize(qint64 maxSize) { m_maxSize = maxSize; }
+
+	/**
+	 * Set the expected type of the file
+	 *
+	 * This must be called before *start*
+	 *
+	 * @param mimetype
+	 */
+	void setExpectedType(const QString &mimetype) { m_expectedType = mimetype; }
+
+	/**
+	 * Set the expected checksum of the file
+	 *
+	 * This must be called before *start*
+	 *
+	 * @param hash the hash (in binary form)
+	 * @param algorithm hashing algorithm used
+	 */
+	void setExpectedHash(const QByteArray &hash, QCryptographicHash::Algorithm algorithm);
+
+	//! Start downloading
+	void start(const QUrl &url);
+
+	/**
+	 * Get access to the file device
+	 *
+	 * This is either a temporary file, a buffer or---if a target filename was given, a regular QFile.
+	 *
+	 * Do not call until finished() is emitted with no error.
+	 */
+	QIODevice *file() { return m_file; }
+
+signals:
+	//! Download progress report
+	void progress(qint64 received, qint64 total);
+
+	//! Download finished. Error message will be non-empty if the download failed
+	void finished(const QString &errorMessage);
+
+private slots:
+	void onMetaDataChanged();
+	void onReadyRead();
+	void onFinished();
+
+private:
+	QFileDevice *m_file;
+	QNetworkReply *m_reply;
+	QString m_expectedType;
+	QByteArray m_expectedHash;
+	qint64 m_maxSize;
+	qint64 m_size;
+
+	QScopedPointer<QCryptographicHash> m_hash;
+};
 }
 
 #endif
