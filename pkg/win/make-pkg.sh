d15772abd07ddc05750162b528e591ab071c465c
Calle Laakkonen <laakkonenc@gmail.com>
2021-01-03 18:40:22 +0200

    Windows: package new translations

--- pkg/win/make-pkg.sh
+++ pkg/win/make-pkg.sh
@@ -100,7 +100,7 @@ cp /Drawpile/desktop/nightmode.colors .
 
 # Copy translations
 mkdir -p i18n
-for tr in cs de fi ja ru zh vi uk it fr
+for tr in cs de fi fr it ja pt ru uk vi zh
 do
 	cp "$QROOT/translations/qt_$tr.qm" i18n/ || true
 	cp "/Build/src/libclient/drawpile_$tr.qm" i18n/
