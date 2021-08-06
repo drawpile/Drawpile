b46b130fd1cafe21fca51046128e638dd3dd73ce
Calle Laakkonen <laakkonenc@gmail.com>
2021-08-06 13:47:53 +0300

    Fix bundled ColorWidgets windows build

--- src/desktop/bundled/QtColorWidgets/include/QtColorWidgets/colorwidgets_global.hpp
+++ src/desktop/bundled/QtColorWidgets/include/QtColorWidgets/colorwidgets_global.hpp
@@ -3,12 +3,6 @@
 
 #include <QtCore/QtGlobal>
 
-#if defined(QTCOLORWIDGETS_STATICALLY_LINKED)
-#    define QCP_EXPORT
-#elif defined(QTCOLORWIDGETS_LIBRARY)
-#    define QCP_EXPORT Q_DECL_EXPORT
-#else
-#    define QCP_EXPORT Q_DECL_IMPORT
-#endif
+#define QCP_EXPORT
 
 #endif // QT_COLOR_WIDGETS_GLOBAL_H
