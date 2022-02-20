c5710836d389f7a0acb90d3b27f0f61bba60268a
askmeaboutloom <askmeaboutloom@live.de>
2022-02-20 17:48:20 +0100

    Remove errant GL check stuff from dpconfig.cmake
    It doesn't belong there, dpconfig.h doesn't contain anything with
    regards to the GL checks anyway.

--- cmake/dpconfig.cmake
+++ cmake/dpconfig.cmake
@@ -1,10 +1,2 @@
-if(BUILD_WITH_GL_CHECKS)
-    set(DP_GL_CHECKS ON)
-else()
-    set(DP_GL_CHECKS OFF)
-endif()
-
-message(STATUS "DP_GL_CHECKS is ${DP_GL_CHECKS}")
-
 set(dpconfig_h "${CMAKE_BINARY_DIR}/config/dpconfig.h")
 configure_file(cmake/dpconfig.tpl.h "${dpconfig_h}")
