6d12f732394c16a56d6fb766a0d5d1c9c12204ff
askmeaboutloom <askmeaboutloom@live.de>
2022-06-16 09:53:41 +0200

    Add a transparent background recording test
    To make sure PNGs are written with unpremultiplied pixels.

--- libengine/test/render_recording.c
+++ libengine/test/render_recording.c
@@ -97,10 +97,14 @@ static void test_render_recording(void **state)
 int main(void)
 {
     const struct CMUnitTest tests[] = {
-        recording_unit_test("brushmodes"), recording_unit_test("layermodes"),
-        recording_unit_test("layerops"),   recording_unit_test("persp"),
-        recording_unit_test("rect"),       recording_unit_test("resize"),
+        recording_unit_test("brushmodes"),
+        recording_unit_test("layermodes"),
+        recording_unit_test("layerops"),
+        recording_unit_test("persp"),
+        recording_unit_test("rect"),
+        recording_unit_test("resize"),
         recording_unit_test("transform"),
+        recording_unit_test("transparentbackground"),
     };
     return cmocka_run_group_tests(tests, NULL, NULL);
 }
