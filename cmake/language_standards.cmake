8b37ccdb05b8b09babaa958206506356428d1c5b
askmeaboutloom <askmeaboutloom@live.de>
2022-12-01 02:27:48 +0100

    Downgrade language standards to C11 and C++11
    To make this buildable with older compilers and CMake versions. Not like
    we use any C17 or C++17 features anyway.

--- cmake/language_standards.cmake
+++ cmake/language_standards.cmake
@@ -20,6 +20,6 @@
 
 function(set_language_standards target)
     set_target_properties("${target}" PROPERTIES
-            C_STANDARD 17 C_STANDARD_REQUIRED ON C_EXTENSIONS OFF
-            CXX_STANDARD 17 CXX_STANDARD_REQUIRED ON CXX_EXTENSIONS OFF)
+            C_STANDARD 11 C_STANDARD_REQUIRED ON C_EXTENSIONS OFF
+            CXX_STANDARD 11 CXX_STANDARD_REQUIRED ON CXX_EXTENSIONS OFF)
 endfunction()
