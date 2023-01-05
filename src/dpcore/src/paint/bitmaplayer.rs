a627052a462b89777f66f81168a7f8ef5e41a408
Colin Snover <github.com@zetafleet.com>
2023-01-05 01:27:46 -0600

    Fix crashes when sampling the edge of a layer

--- src/dpcore/src/paint/bitmaplayer.rs
+++ src/dpcore/src/paint/bitmaplayer.rs
@@ -371,7 +371,7 @@ impl BitmapLayer {
     /// Get a weighted average of the color using a default sampling mask
     pub fn sample_color(&self, x: i32, y: i32, dia: i32) -> Color {
         if dia <= 1 {
-            if x < 0 || y < 0 || x as u32 > self.width || y as u32 > self.height {
+            if x < 0 || y < 0 || x as u32 >= self.width || y as u32 >= self.height {
                 Color::TRANSPARENT
             } else {
                 Color::from_pixel15(self.pixel_at(x as u32, y as u32))
