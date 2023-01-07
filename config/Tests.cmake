bc58ea7f1bc370709eda7b762ef2e09a5dd762fc
Colin Snover <github.com@zetafleet.com>
2023-01-06 23:39:11 -0600

    Rewrite CMake files
    This commit replaces most of the “old” CMake style in the build
    scripts with “modern” CMake in order to make the build system
    more maintainable and also to make it work with multi-config
    generators.
    
    Hacks to use old deprecated CMake policies, non-idiomatic macros,
    custom command targets, etc. have all been removed where possible
    and replaced with built-in CMake features. Misuse of CMake features
    that only work with single-target generators have been replaced with
    appropriate generator expressions, source property settings, or
    removed. (Notably, builds using single-config generators will not
    default to Release any more if the build type is unspecified, and
    will instead require an explicit build type to be given with
    `-DCMAKE_BUILD_TYPE`.)
    
    Generated Visual Studio and Xcode projects now have logical source
    groups that match the project directory structure. To avoid making
    this even more painful than necessary, desktop assets are moved
    from the root into the source tree.
    
    In-source builds are now prohibited to reduce the risk of anyone
    accidentally blowing anything away or checking in generated files
    to version control.
    
    Drawpile version numbers have all been updated to use the
    workspace Cargo.toml as a single source of truth (along with other
    information duplicated in Cargo.toml). Drawpile protocol version
    has also been moved into dpcore and is extracted from there for
    use on the C++ side. Version numbers now follow the semver
    standard.
    
    The dependency on Extra CMake Modules is removed as it was barely
    used, the functionality was not complicated to reproduce, and
    the most complex parts being used by Drawpile do not even work
    properly with Qt6.
    
    The custom Rust/Cargo integration code has been replaced with a
    simplified and hopefully slightly more correct one.
    
    More assets now are generated from source when it was trivial to
    do so (e.g. macOS .icns). Unnecessary intermediate rasters have
    been removed, and vector art has been optimised so it is not
    necessary to use binary compression.
    
    Copyright years are removed from the build system as they were
    outdated in many areas and legally unnecessary, and invalid SPDX
    license values have been replaced with the correct ones.
    
    CMake now provides more useful output about which features are
    enabled or not during project generation, and checks Qt version
    numbers to fail fast in environments with outdated Qt versions.
    
    Bespoke executable debug symbol stripping has been removed, as
    this did not work correctly with multi-target generators, and there
    is a standard way to do this (--strip).
    
    Unit test macros have been replaced with a function that accepts
    idiomatic arguments.

--- config/Tests.cmake
+++ config/Tests.cmake
@@ -1,16 +1,52 @@
-macro ( AddUnitTest test )
-	add_executable("test_${TEST_PREFIX}_${test}" "${test}.cpp" ${TestResources})
-	target_link_libraries("test_${TEST_PREFIX}_${test}" ${TEST_LIBS})
+function(add_unit_tests prefix)
+	set(multiValueArgs LIBS SOURCES TESTS)
+	cmake_parse_arguments(PARSE_ARGV 1 ARG "" "" "${multiValueArgs}")
+
+	if(NOT prefix)
+		message(FATAL_ERROR "missing required prefix")
+		return()
+	endif()
+
+	foreach(name IN LISTS ARG_TESTS)
+		if(name)
+			add_unit_test("${prefix}" "${name}" LIBS ${ARG_LIBS} SOURCES ${ARG_SOURCES})
+		endif()
+	endforeach()
+endfunction()
+
+function(add_unit_test prefix name)
+	set(multiValueArgs LIBS SOURCES)
+	cmake_parse_arguments(PARSE_ARGV 2 ARG "" "" "${multiValueArgs}")
+
+	if(NOT prefix)
+		message(FATAL_ERROR "missing required prefix")
+		return()
+	endif()
+
+	if(NOT name)
+		message(FATAL_ERROR "missing required name")
+		return()
+	endif()
+
+	set(test_name "${prefix}_${name}")
+	set(target_name "test_${test_name}")
+
+	add_executable(${target_name}
+		"${name}.cpp"
+		${ARG_SOURCES}
+	)
+	target_include_directories(${target_name} PRIVATE ..)
+	target_link_libraries(${target_name} ${ARG_LIBS})
 
 	add_test(
-		NAME "${TEST_PREFIX}_${test}"
-		COMMAND "test_${TEST_PREFIX}_${test}"
+		NAME ${test_name}
+		COMMAND ${target_name}
 	)
 
 	# Use the offscreen platform module so tests can run headlessly
 	set_property(
-		TEST "${TEST_PREFIX}_${test}" APPEND PROPERTY
+		TEST ${test_name}
+		APPEND PROPERTY
 		ENVIRONMENT QT_QPA_PLATFORM=offscreen
 	)
-endmacro()
-
+endfunction()
