6df134dae5d863beef4f6e7b20c57117d7e70e79
askmeaboutloom <askmeaboutloom@live.de>
2023-02-01 16:27:48 +0100

    Implement zip reading for libzip implementation
    For Qt6 Drawpile, since using KArchive is troublesome there.

--- libengine/dpengine/zip_archive_libzip.c
+++ libengine/dpengine/zip_archive_libzip.c
@@ -19,6 +19,14 @@
 #include <zip.h>
 
 
+#define INITIAL_READ_CAPACITY 4098
+
+typedef struct DP_ZipReaderFile {
+    size_t size;
+    unsigned char buffer[];
+} DP_ZipReaderFile;
+
+
 static const char *code_strerror(zip_error_t *ze)
 {
     if (ze) {
@@ -33,6 +41,102 @@ static const char *code_strerror(zip_error_t *ze)
 static const char *archive_strerror(zip_t *archive)
 {
     return code_strerror(zip_get_error(archive));
+}
+
+static const char *file_strerror(zip_file_t *file)
+{
+    return code_strerror(zip_file_get_error(file));
+}
+
+static void file_close(zip_file_t *file, const char *path)
+{
+    int errcode = zip_fclose(file);
+    if (errcode != ZIP_ER_OK) {
+        zip_error_t ze;
+        zip_error_init_with_code(&ze, errcode);
+        DP_warn("Error closing '%s' in zip: %s", path, code_strerror(&ze));
+    }
+}
+
+
+DP_ZipReader *DP_zip_reader_new(const char *path)
+{
+    int errcode;
+    zip_t *archive = zip_open(path, ZIP_RDONLY, &errcode);
+    if (archive) {
+        return (DP_ZipReader *)archive;
+    }
+    else {
+        zip_error_t ze;
+        zip_error_init_with_code(&ze, errcode);
+        DP_error_set("Error opening '%s': %s", path, code_strerror(&ze));
+        zip_error_fini(&ze);
+        return NULL;
+    }
+}
+
+void DP_zip_reader_free(DP_ZipReader *zr)
+{
+    zip_t *archive = (zip_t *)zr;
+    zip_discard(archive);
+}
+
+DP_ZipReaderFile *DP_zip_reader_read_file(DP_ZipReader *zr, const char *path)
+{
+    zip_t *archive = (zip_t *)zr;
+    zip_file_t *file = zip_fopen(archive, path, 0);
+    if (!file) {
+        DP_error_set("Error opening file '%s' in zip: %s", path,
+                     archive_strerror(archive));
+        return NULL;
+    }
+
+    size_t capacity = INITIAL_READ_CAPACITY;
+    size_t used = 0;
+    DP_ZipReaderFile *zrf =
+        DP_malloc(DP_FLEX_SIZEOF(DP_ZipReaderFile, buffer, capacity));
+    while (true) {
+        size_t expect = capacity - used;
+        zip_int64_t result =
+            zip_fread(file, zrf->buffer + used, (zip_uint64_t)expect);
+        if (result < 0) {
+            DP_error_set("Error reading from file '%s' in zip: %s", path,
+                         file_strerror(file));
+            DP_free(zrf);
+            file_close(file, path);
+            return NULL;
+        }
+        else {
+            size_t read = (size_t)result;
+            used += read;
+            if (read < expect) {
+                zrf->size = used;
+                file_close(file, path);
+                return zrf;
+            }
+            else {
+                capacity *= 2;
+                zrf = DP_realloc(
+                    zrf, DP_FLEX_SIZEOF(DP_ZipReaderFile, buffer, capacity));
+            }
+        }
+    }
+}
+
+
+size_t DP_zip_reader_file_size(DP_ZipReaderFile *zrf)
+{
+    return zrf->size;
+}
+
+void *DP_zip_reader_file_content(DP_ZipReaderFile *zrf)
+{
+    return zrf->buffer;
+}
+
+void DP_zip_reader_file_free(DP_ZipReaderFile *zrf)
+{
+    DP_free(zrf);
 }
 
 
