84be18e00c49db986783cf363248fada7e413e95
Colin Snover <github.com@zetafleet.com>
2023-03-21 19:38:36 -0500

    Fix notification pop-up not repainting contents on every change
    (cherry picked from commit c375a9de558fc66ba71cbcc89c5fe3bd0107a2d1)

--- src/desktop/widgets/popupmessage.cpp
+++ src/desktop/widgets/popupmessage.cpp
@@ -105,7 +105,10 @@ void PopupMessage::showMessage(const QPoint& point, const QString &message)
 	// Redraw the background and show on screen
 	redrawBubble();
 	move(rect.topLeft());
-	show();
+	if (isVisible())
+		update();
+	else
+		show();
 	m_timer->start();
 }
 
