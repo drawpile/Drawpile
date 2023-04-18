33a518caa301d1d57cf9bde978a99ff80e83c2e0
askmeaboutloom <askmeaboutlo0m@users.noreply.github.com>
2023-04-19 00:54:57 +0200

    Use Github CLI in Github release script
    Instead of manually hitting the API through curl, cutting down the bulk
    of the script into something much shorter.

--- pkg/make-github-release.sh
+++ pkg/make-github-release.sh
@@ -7,11 +7,6 @@ note() {
     echo "$@" 1>&2
 }
 
-die() {
-    note "$@"
-    exit 1
-}
-
 check_args() {
     local error fields var
     error=0
@@ -55,133 +50,38 @@ check_args() {
 
 check_args "$@"
 
-BASE_URL="${BASE_URL:-https://api.github.com}"
 GIT_REPO_SLUG="${GIT_REPO_SLUG:-drawpile/Drawpile}"
 
-prepare_assets() {
-    local dir pattern path count
-    while [[ $# -ne 0 ]]; do
-        IFS=';' read -ra fields <<< "$1"
-        shift
+assets=()
+error=0
+while [[ $# -ne 0 ]]; do
+    IFS=';' read -ra fields <<< "$1"
+    shift
 
-        dir="${fields[0]}"
-        pattern="${fields[1]}"
-        path="$(find "$dir" -name "$pattern")"
-        count="$(echo "$path" | wc -l)"
-        if [[ $count -ne 1 ]]; then
-            die "$dir: expected 1 path matching '$pattern', but got $count"
-        fi
+    dir="${fields[0]}"
+    pattern="${fields[1]}"
+    path="$(find "$dir" -name "$pattern")"
+    count="$(echo "$path" | wc -l)"
+    if [[ $count -ne 1 ]]; then
+        note "$dir: expected 1 path matching '$pattern', but got $count"
+        error=1
+    fi
 
-        # Write out path and label pairs, later processed by upload_assets.
-        echo "$path"
-        echo "${fields[2]}"
-    done
-}
+    assets+=("$path#${fields[2]}")
+done
 
-api_call() {
-    local method url
-    method="$1"
-    url="$2"
-    shift 2
-    curl --no-progress-meter -L -X "$method" \
-        -H "Accept: application/vnd.github+json" \
-        -H "Authorization: token $GITHUB_TOKEN"\
-        -H "X-GitHub-Api-Version: 2022-11-28" \
-        "$@" "$url" | jq -MS
-}
-
-get_existing_release() {
-    note "Get release '$RELEASE_NAME'"
-    api_call GET "$BASE_URL/repos/$GIT_REPO_SLUG/releases/tags/$RELEASE_NAME"
-}
-
-delete_existing_release() {
-    local release_id
-    release_id="$1"
-    note "Delete release with id '$release_id'"
-    api_call DELETE "$BASE_URL/repos/$GIT_REPO_SLUG/releases/$release_id"
-}
-
-delete_existing_tag() {
-    note "Delete tag '$RELEASE_NAME'"
-    api_call DELETE "$BASE_URL/repos/$GIT_REPO_SLUG/git/refs/tags/$RELEASE_NAME"
-}
-
-create_release() {
-    local json
-    note "Create release '$RELEASE_NAME' on commit '$TARGET_COMMIT'"
-    json="$(jq -n \
-        --arg name "$RELEASE_NAME" \
-        --arg commit "$TARGET_COMMIT" \
-        --arg body "$RELEASE_DESCRIPTION" \
-        '{
-            "body": $body,
-            "draft": true,
-            "name": $name,
-            "prerelease": true,
-            "tag_name": $name,
-            "target_commitish": $commit
-        }')"
-    api_call POST "$BASE_URL/repos/$GIT_REPO_SLUG/releases" -d "$json"
-}
-
-urlencode() {
-    printf '%s' "$1" | jq -sRr '@uri'
-}
-
-create_release_asset() {
-    local url path label name
-    url="$1"
-    path="$2"
-    label="$3"
-    name="$(basename "$path")"
-    note "Create release asset named '$name' labeled '$label' from '$path'"
-    api_call POST "$url?name=$(urlencode "$name")&label=$(urlencode "$label")" \
-        -H "Content-Type: application/octet-stream" \
-        --data-binary "@$path"
-}
-
-upload_assets() {
-    local url
-    url="$1"
-    # Read two lines at a time, first line is the path, second is the label.
-    while mapfile -t -n 2 pair && [[ ${#pair[@]} -ne 0 ]]; do
-        create_release_asset "$url" "${pair[0]}" "${pair[1]}"
-    done
-}
-
-publish_release() {
-    local release_id
-    release_id="$1"
-    note "Publish release with id '$release_id'"
-    api_call PATCH "$BASE_URL/repos/$GIT_REPO_SLUG/releases/$release_id" \
-        -d '{"draft":false}'
-}
-
-# Check if all assets are present. The result will be pairs of lines, the first
-# line in each pair is the path, the second is the label.
-assets_to_upload="$(prepare_assets "$@")"
-
-# If we already have a release of the same name, we replace it.
-existing_release="$(get_existing_release)"
-if echo "$existing_release" | jq -e .id >/dev/null; then
-    existing_release_id="$(echo "$existing_release" | jq -r .id)"
-    delete_existing_release "$existing_release_id"
+if [[ $error -ne 0 ]]; then
+    note 'Error(s) collecting release assets'
+    exit 1
 fi
 
-# Don't bother to check if the tags exists, just try to delete it.
-delete_existing_tag
+gh release delete --repo "$GIT_REPO_SLUG" --cleanup-tag --yes "$RELEASE_NAME" \
+    || true
 
-# Create the release in draft mode.
-release="$(create_release)"
-if echo "$release" | jq -e .upload_url >/dev/null; then
-    # The release contains a URL to use for uploading assets to, but it has some
-    # some hypermedia "garbage" in braces at the end that we need to strip off.
-    upload_url="$(echo "$release" | jq -r .upload_url | perl -pe 's/\{.*\}$//')"
-    echo "$assets_to_upload" | upload_assets "$upload_url"
-    # Switch the release over from draft to published.
-    release_id="$(echo "$release" | jq -r .id)"
-    publish_release "$release_id"
-else
-    die 'No upload_url in release response, release creation probably failed'
-fi
+gh release create \
+    --repo "$GIT_REPO_SLUG" \
+    --prerelease \
+    --target "$TARGET_COMMIT" \
+    --title "$RELEASE_NAME" \
+    --notes "$RELEASE_DESCRIPTION" \
+    "$RELEASE_NAME" "${assets[@]}"
