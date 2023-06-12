121fceea01607eeaa50811c1af088a5b1c202634
askmeaboutloom <askmeaboutlo0m@users.noreply.github.com>
2023-06-12 19:42:39 +0200

    Disable version checking by default
    It's not something that makes sense for anything other than official
    releases, and even there it needs some looking at first, to make them
    work for Linux and Android too.

--- cmake/DrawpileOptions.cmake
+++ cmake/DrawpileOptions.cmake
@@ -48,7 +48,7 @@ endif()
 option(DIST_BUILD "Build for stand-alone distribution")
 add_feature_info("Distribution build (DIST_BUILD)" DIST_BUILD "")
 
-option(VERSION_CHECK "Enable code to check for updates" ON)
+option(VERSION_CHECK "Enable code to check for updates" OFF)
 add_feature_info("Automatic update checking code (VERSION_CHECK)" VERSION_CHECK "")
 
 # Feature info will be emitted later once it is clear whether or not these
