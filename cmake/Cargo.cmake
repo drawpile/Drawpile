0e8c628624c3389ce364959ea75bf0e57f52eb47
askmeaboutloom <askmeaboutlo0m@users.noreply.github.com>
2023-08-31 12:21:44 +0200

    Update Cargo.cmake to work with latest Rust
    Steals the linked library detection from Carbon again, since that stuff
    changed and broke everything. This also removes the ability to produce
    Rust executables directly, only libraries are supported now. This avoids
    the mess of passing libraries to link to Cargo, instead just produce a
    static library and add a dummy executable that calls into it, doing the
    linking through cmake.

--- cmake/Cargo.cmake
+++ cmake/Cargo.cmake
@@ -1,27 +1,92 @@
-#[[
-Adds an imported Rust executable target that is built by Cargo.
-#]]
-function(add_cargo_executable target)
-	_add_cargo(${target} TRUE ${ARGN})
+# SPDX-License-Identifier: MIT
+find_program(CARGO_COMMAND cargo REQUIRED
+	PATHS $ENV{HOME}/.cargo/bin
+	DOC "Cargo executable"
+)
+
+# SPDX-SnippetBegin
+# SPDX-License-Identifier: MIT
+# SDPX—SnippetName: static libs detection from corrosion
+function(_corrosion_determine_libs_new target_triple out_libs)
+    set(package_dir "${CMAKE_BINARY_DIR}/corrosion/required_libs")
+    # Cleanup on reconfigure to get a cleans state (in case we change something in the future)
+    file(REMOVE_RECURSE "${package_dir}")
+    file(MAKE_DIRECTORY "${package_dir}")
+    set(manifest "[package]\nname = \"required_libs\"\nedition = \"2018\"\nversion = \"0.1.0\"\n")
+    string(APPEND manifest "\n[lib]\ncrate-type=[\"staticlib\"]\npath = \"lib.rs\"\n")
+    string(APPEND manifest "\n[workspace]\n")
+    file(WRITE "${package_dir}/Cargo.toml" "${manifest}")
+    file(WRITE "${package_dir}/lib.rs" "pub fn add(left: usize, right: usize) -> usize {left + right}\n")
+
+	set(args rustc --verbose --color never)
+	if(target_triple)
+		list(APPEND args --target=${target_triple})
+	endif()
+	list(APPEND args -- --print=native-static-libs)
+	execute_process(
+		COMMAND ${CARGO_COMMAND} ${args}
+		WORKING_DIRECTORY "${CMAKE_BINARY_DIR}/corrosion/required_libs"
+		RESULT_VARIABLE cargo_build_result
+		ERROR_VARIABLE cargo_build_error_message
+	)
+
+    if(cargo_build_result)
+        message(SEND_ERROR "Determining required native libraries - failed: ${cargo_build_result}.")
+        message(SEND_ERROR "The cargo build error was: ${cargo_build_error_message}")
+        return()
+    else()
+        # The pattern starts with `native-static-libs:` and goes to the end of the line.
+        if(cargo_build_error_message MATCHES "native-static-libs: ([^\r\n]+)\r?\n")
+            string(REPLACE " " ";" "libs_list" "${CMAKE_MATCH_1}")
+            set(stripped_lib_list "")
+            foreach(lib ${libs_list})
+                # Strip leading `-l` (unix) and potential .lib suffix (windows)
+                string(REGEX REPLACE "^-l" "" "stripped_lib" "${lib}")
+                string(REGEX REPLACE "\.lib$" "" "stripped_lib" "${stripped_lib}")
+                list(APPEND stripped_lib_list "${stripped_lib}")
+            endforeach()
+            set(libs_list "${stripped_lib_list}")
+            # Special case `msvcrt` to link with the debug version in Debug mode.
+            list(TRANSFORM libs_list REPLACE "^msvcrt$" "\$<\$<CONFIG:Debug>:msvcrtd>")
+        else()
+            message(SEND_ERROR "Determining required native libraries - failed: Regex match failure.")
+            message(SEND_ERROR "`native-static-libs` not found in: `${cargo_build_error_message}`")
+            return()
+        endif()
+    endif()
+    set("${out_libs}" "${libs_list}" PARENT_SCOPE)
+endfunction()
+# SPDX-SnippetEnd
+
+function(_cargo_set_libs target triple)
+	if(NOT "${triple}")
+		set(cache_var "CARGO_LIBS_${triple}")
+	else()
+		set(cache_var "CARGO_LIBS_HOST")
+	endif()
+
+	set(libs "${${cache_var}}")
+	if(libs)
+		message(STATUS "Re-used Rust libraries: ${libs}")
+	else()
+		_corrosion_determine_libs_new("${triple}" libs)
+		set("${cache_var}" "${libs}" CACHE INTERNAL "")
+		message(STATUS "Determined Rust libraries: ${libs}")
+	endif()
+
+	set_target_properties("${target}" PROPERTIES
+		INTERFACE_LINK_LIBRARIES "${libs}"
+	)
 endfunction()
 
 #[[
 Adds an imported Rust static library target that is built by Cargo.
 #]]
-function(add_cargo_library target)
-	_add_cargo(${target} FALSE ${ARGN})
-endfunction()
-
-function(_add_cargo target is_exe)
+function(add_cargo_library target package)
 	set(options ALL_FEATURES NO_DEFAULT_FEATURES)
-	set(singleValueArgs PACKAGE MANIFEST_PATH)
+	set(singleValueArgs MANIFEST_PATH)
 	set(multiValueArgs FEATURES)
 	cmake_parse_arguments(PARSE_ARGV 2 ARG "${options}" "${singleValueArgs}" "${multiValueArgs}")
-
-	find_program(CARGO_COMMAND cargo REQUIRED
-		PATHS $ENV{HOME}/.cargo/bin
-		DOC "Cargo executable"
-	)
 
 	if(CMAKE_CROSSCOMPILING)
 		if(CMAKE_SYSTEM_PROCESSOR MATCHES "ARM64|[Aa][Aa][Rr][Cc][Hh]64")
@@ -62,28 +127,14 @@ function(_add_cargo target is_exe)
 	set(cargo_dir ${CMAKE_CURRENT_BINARY_DIR}/cargo)
 	set(cargo_target_dir "${cargo_dir}/$<$<BOOL:${triple}>:${triple}/>$<LOWER_CASE:$<CONFIG>>")
 
-	if(is_exe)
-		set(all ALL)
-		set(out_name ${target}${CMAKE_EXECUTABLE_SUFFIX})
-		set(out_kind RUNTIME)
-	else()
-		set(all "")
-		set(out_name ${CMAKE_STATIC_LIBRARY_PREFIX}${ARG_PACKAGE}${CMAKE_STATIC_LIBRARY_SUFFIX})
-		set(out_kind ARCHIVE)
-	endif()
-
-	if(CMAKE_${out_kind}_OUTPUT_DIRECTORY)
-		set(out_dir ${CMAKE_${out_kind}_OUTPUT_DIRECTORY})
+	set(out_name ${CMAKE_STATIC_LIBRARY_PREFIX}${package}${CMAKE_STATIC_LIBRARY_SUFFIX})
+	if(CMAKE_ARCHIVE_OUTPUT_DIRECTORY)
+		set(out_dir ${CMAKE_ARCHIVE_OUTPUT_DIRECTORY})
 	else()
 		set(out_dir ${CMAKE_CURRENT_BINARY_DIR})
 	endif()
 
-	if(ARG_PACKAGE)
-		set(extra_comment " with packages ${ARG_PACKAGE}")
-	endif()
-
 	add_custom_target(cargo-build_${target}
-		${all}
 		BYPRODUCTS "${out_dir}/${out_name}"
 		COMMAND ${CMAKE_COMMAND} -E env MACOSX_DEPLOYMENT_TARGET=${CMAKE_OSX_DEPLOYMENT_TARGET}
 			${CARGO_COMMAND} build
@@ -92,13 +143,13 @@ function(_add_cargo target is_exe)
 			"$<$<BOOL:${ARG_ALL_FEATURES}>:--all-features>"
 			"$<$<BOOL:${ARG_NO_DEFAULT_FEATURES}>:--no-default-features>"
 			"$<$<BOOL:${triple}>:--target;${triple}>"
-			"$<$<BOOL:${ARG_PACKAGE}>:--package;${ARG_PACKAGE}>"
+			"$<$<BOOL:${package}>:--package;${package}>"
 			"$<$<BOOL:${VERBOSE}>:--verbose>"
 			--profile "$<IF:$<CONFIG:Debug>,dev,$<LOWER_CASE:$<CONFIG>>>"
 			--target-dir ${cargo_dir}
 		COMMAND ${CMAKE_COMMAND} -E copy_if_different "${cargo_target_dir}/${out_name}" "${out_dir}/${out_name}"
 		COMMAND_EXPAND_LISTS
-		COMMENT "Running cargo${extra_comment}"
+		COMMENT "Running cargo with package ${package}"
 		VERBATIM
 	)
 
@@ -106,43 +157,17 @@ function(_add_cargo target is_exe)
 	# rules about finding package directories and it does not work if someone
 	# does not specify packages at all but it works for how this function is
 	# used today, so YAGNI
-	if(ARG_PACKAGE AND CMAKE_VERSION VERSION_GREATER_EQUAL 3.20)
-		foreach(item IN LISTS ARG_PACKAGE)
+	if(CMAKE_VERSION VERSION_GREATER_EQUAL 3.20)
+		foreach(item IN LISTS package)
 			file(GLOB_RECURSE sources ${PROJECT_SOURCE_DIR}/src/${item}/*)
 			target_sources(cargo-build_${target} PRIVATE ${sources})
 			source_group(TREE "${PROJECT_SOURCE_DIR}/src/${item}" PREFIX "Source Files" FILES ${sources})
 		endforeach()
 	endif()
 
-	if(is_exe)
-		add_executable(${target} IMPORTED GLOBAL)
-	else()
-		add_library(${target} STATIC IMPORTED GLOBAL)
+	add_library(${target} STATIC IMPORTED GLOBAL)
 
-		# Adapted from corrosion. Required for successful linking of anything
-		# that uses the Rust Standard Library
-		set(libs "")
-		if(WIN32)
-			list(APPEND libs "advapi32" "userenv" "ws2_32" "bcrypt")
-			if(MSVC)
-				list(APPEND libs "$<$<CONFIG:Debug>:msvcrtd>")
-				# CONFIG takes a comma seperated list starting with CMake 3.19, but we still need to
-				# support older CMake versions.
-				set(config_is_release "$<OR:$<CONFIG:Release>,$<CONFIG:MinSizeRel>,$<CONFIG:RelWithDebInfo>>")
-				list(APPEND libs "$<${config_is_release}:msvcrt>")
-			else()
-				list(APPEND libs "gcc_eh" "pthread")
-			endif()
-		elseif(APPLE)
-			list(APPEND libs "System" "resolv" "c" "m")
-		else()
-			list(APPEND libs "dl" "rt" "pthread" "gcc_s" "c" "m" "util")
-		endif()
-
-		set_target_properties(${target} PROPERTIES
-			INTERFACE_LINK_LIBRARIES "${libs}"
-		)
-	endif()
+	_cargo_set_libs("${target}" "${triple}")
 
 	add_dependencies(${target} cargo-build_${target})
 	set_target_properties(${target} PROPERTIES
