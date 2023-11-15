4e735684be8a8d1cc62a59fa35a627f1d5713108
askmeaboutloom <askmeaboutlo0m@users.noreply.github.com>
2023-11-15 11:07:57 +0100

    Auto-format qtguicompat.h

--- src/desktop/utils/qtguicompat.h
+++ src/desktop/utils/qtguicompat.h
@@ -1,11 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
-// SPDX-FileCopyrightText: Drawpile contributors
-
 #ifndef DP_DESKTOP_UTILS_QTGUICOMPAT_H
 #define DP_DESKTOP_UTILS_QTGUICOMPAT_H
-
 #include <QApplication>
-#include <QtGlobal>
 #include <QDragMoveEvent>
 #include <QDropEvent>
 #include <QKeyEvent>
@@ -16,61 +12,72 @@
 #include <QTouchEvent>
 #include <QWheelEvent>
 #include <QWidget>
+#include <QtGlobal>
 #if QT_VERSION >= QT_VERSION_CHECK(6, 0, 0)
-#include <QEventPoint>
-#include <QInputDevice>
+#	include <QEventPoint>
+#	include <QInputDevice>
 #endif
 
 namespace compat {
 #if QT_VERSION >= QT_VERSION_CHECK(5, 13, 0)
-inline auto fontFamily(const QTextCharFormat &format) {
+inline auto fontFamily(const QTextCharFormat &format)
+{
 	return format.fontFamilies().toStringList().value(0, QString{});
 }
 
-inline void setFontFamily(QTextCharFormat &format, const QString &family) {
-	format.setFontFamilies({ family });
+inline void setFontFamily(QTextCharFormat &format, const QString &family)
+{
+	format.setFontFamilies({family});
 }
 #else
-inline auto fontFamily(const QTextCharFormat &format) {
+inline auto fontFamily(const QTextCharFormat &format)
+{
 	return format.fontFamily();
 }
 
-inline void setFontFamily(QTextCharFormat &format, const QString &family) {
+inline void setFontFamily(QTextCharFormat &format, const QString &family)
+{
 	format.setFontFamily(family);
 }
 #endif
 
 #if QT_VERSION >= QT_VERSION_CHECK(5, 14, 0)
-inline auto widgetScreen(const QWidget &widget) {
+inline auto widgetScreen(const QWidget &widget)
+{
 	return widget.screen();
 }
 
-inline auto wheelPosition(const QWheelEvent &event) {
+inline auto wheelPosition(const QWheelEvent &event)
+{
 	return event.position();
 }
 #else
-inline auto widgetScreen(const QWidget &) {
+inline auto widgetScreen(const QWidget &)
+{
 	return qApp->primaryScreen();
 }
 
-inline auto wheelPosition(const QWheelEvent &event) {
+inline auto wheelPosition(const QWheelEvent &event)
+{
 	return event.posF();
 }
 #endif
 
 #if QT_VERSION >= QT_VERSION_CHECK(5, 15, 0)
-inline auto tabDevice(const QTabletEvent &event) {
+inline auto tabDevice(const QTabletEvent &event)
+{
 	return event.deviceType();
 }
 #else
-inline auto tabDevice(const QTabletEvent &event) {
+inline auto tabDevice(const QTabletEvent &event)
+{
 	return event.device();
 }
 #endif
 
 #if QT_VERSION >= QT_VERSION_CHECK(6, 0, 0)
-#define HAVE_QT_COMPAT_DEFAULT_HIGHDPI_PIXMAPS
-#define HAVE_QT_COMPAT_DEFAULT_DISABLE_WINDOW_CONTEXT_HELP_BUTTON
+#	define HAVE_QT_COMPAT_DEFAULT_HIGHDPI_PIXMAPS
+#	define HAVE_QT_COMPAT_DEFAULT_DISABLE_WINDOW_CONTEXT_HELP_BUTTON
 
 using DeviceType = QInputDevice::DeviceType;
 using EnterEvent = QEnterEvent;
@@ -81,25 +88,16 @@ const auto FourDMouseDevice = DeviceType::Mouse;
 const auto RotationStylusDevice = DeviceType::Stylus;
 
 inline auto makeTabletEvent(
-	QEvent::Type type,
-	const QPointF &pos,
-	const QPointF &globalPos,
-	compat::DeviceType device,
-	compat::PointerType pointerType,
-	qreal pressure,
-	int xTilt,
-	int yTilt,
-	qreal tangentialPressure,
-	qreal rotation,
-	int z,
-	Qt::KeyboardModifiers keyState,
-	qint64 uniqueID,
-	Qt::MouseButton button,
-	Qt::MouseButtons buttons
-) {
+	QEvent::Type type, const QPointF &pos, const QPointF &globalPos,
+	compat::DeviceType device, compat::PointerType pointerType, qreal pressure,
+	int xTilt, int yTilt, qreal tangentialPressure, qreal rotation, int z,
+	Qt::KeyboardModifiers keyState, qint64 uniqueID, Qt::MouseButton button,
+	Qt::MouseButtons buttons)
+{
 	const QPointingDevice *sysDevice = nullptr;
 	for(const auto *candidate : QInputDevice::devices()) {
-		if(candidate && candidate->type() == device && candidate->systemId() == uniqueID) {
+		if(candidate && candidate->type() == device &&
+		   candidate->systemId() == uniqueID) {
 			const auto *pd = static_cast<const QPointingDevice *>(candidate);
 			if(pd->pointerType() == pointerType) {
 				sysDevice = pd;
@@ -115,46 +113,58 @@ inline auto makeTabletEvent(
 		sysDevice = QPointingDevice::primaryPointingDevice();
 	}
 
-	return ::QTabletEvent(type, sysDevice, pos, globalPos, pressure, xTilt, yTilt, tangentialPressure, rotation, z, keyState, button, buttons);
+	return ::QTabletEvent(
+		type, sysDevice, pos, globalPos, pressure, xTilt, yTilt,
+		tangentialPressure, rotation, z, keyState, button, buttons);
 }
 
-inline auto keyPressed(const QKeyEvent &event) {
+inline auto keyPressed(const QKeyEvent &event)
+{
 	return event.keyCombination();
 }
 
-inline auto globalPos(const QMouseEvent &event) {
+inline auto globalPos(const QMouseEvent &event)
+{
 	return event.globalPosition().toPoint();
 }
 
-inline auto mousePos(const QMouseEvent &event) {
+inline auto mousePos(const QMouseEvent &event)
+{
 	return event.position().toPoint();
 }
 
-inline auto mousePosition(const QMouseEvent &event) {
+inline auto mousePosition(const QMouseEvent &event)
+{
 	return event.position();
 }
 
-inline auto styleName(const QStyle &style) {
+inline auto styleName(const QStyle &style)
+{
 	return style.name();
 }
 
-inline auto tabPosF(const QTabletEvent &event) {
+inline auto tabPosF(const QTabletEvent &event)
+{
 	return event.position();
 }
 
-inline auto touchPoints(const QTouchEvent &event) {
+inline auto touchPoints(const QTouchEvent &event)
+{
 	return event.points();
 }
 
-inline auto touchStartPos(const QEventPoint &event) {
+inline auto touchStartPos(const QEventPoint &event)
+{
 	return event.pressPosition();
 }
 
-inline auto touchLastPos(const QEventPoint &event) {
+inline auto touchLastPos(const QEventPoint &event)
+{
 	return event.lastPosition();
 }
 
-inline auto touchPos(const QEventPoint &event) {
+inline auto touchPos(const QEventPoint &event)
+{
 	return event.position();
 }
 
@@ -182,62 +192,64 @@ const auto FourDMouseDevice = DeviceType::FourDMouse;
 const auto RotationStylusDevice = DeviceType::RotationStylus;
 
 inline auto makeTabletEvent(
-	QEvent::Type type,
-	const QPointF &pos,
-	const QPointF &globalPos,
-	compat::DeviceType device,
-	compat::PointerType pointerType,
-	qreal pressure,
-	int xTilt,
-	int yTilt,
-	qreal tangentialPressure,
-	qreal rotation,
-	int z,
-	Qt::KeyboardModifiers keyState,
-	qint64 uniqueID,
-	Qt::MouseButton button,
-	Qt::MouseButtons buttons
-) {
-	return ::QTabletEvent(type, pos, globalPos, device, pointerType, pressure, xTilt, yTilt, tangentialPressure, rotation, z, keyState, uniqueID, button, buttons);
+	QEvent::Type type, const QPointF &pos, const QPointF &globalPos,
+	compat::DeviceType device, compat::PointerType pointerType, qreal pressure,
+	int xTilt, int yTilt, qreal tangentialPressure, qreal rotation, int z,
+	Qt::KeyboardModifiers keyState, qint64 uniqueID, Qt::MouseButton button,
+	Qt::MouseButtons buttons)
+{
+	return ::QTabletEvent(
+		type, pos, globalPos, device, pointerType, pressure, xTilt, yTilt,
+		tangentialPressure, rotation, z, keyState, uniqueID, button, buttons);
 }
 
-inline auto keyPressed(const QKeyEvent &event) {
+inline auto keyPressed(const QKeyEvent &event)
+{
 	return event.key();
 }
 
-inline auto globalPos(const QMouseEvent &event) {
+inline auto globalPos(const QMouseEvent &event)
+{
 	return event.globalPos();
 }
 
-inline auto mousePos(const QMouseEvent &event) {
+inline auto mousePos(const QMouseEvent &event)
+{
 	return event.pos();
 }
 
-inline auto mousePosition(const QMouseEvent &event) {
+inline auto mousePosition(const QMouseEvent &event)
+{
 	return ::QPointF(event.pos());
 }
 
-inline auto tabPosF(const QTabletEvent &event) {
+inline auto tabPosF(const QTabletEvent &event)
+{
 	return event.posF();
 }
 
-inline auto styleName(const QStyle &style) {
+inline auto styleName(const QStyle &style)
+{
 	return style.objectName();
 }
 
-inline auto touchPoints(const QTouchEvent &event) {
+inline auto touchPoints(const QTouchEvent &event)
+{
 	return event.touchPoints();
 }
 
-inline auto touchStartPos(const QTouchEvent::TouchPoint &event) {
+inline auto touchStartPos(const QTouchEvent::TouchPoint &event)
+{
 	return event.startPos();
 }
 
-inline auto touchLastPos(const QTouchEvent::TouchPoint &event) {
+inline auto touchLastPos(const QTouchEvent::TouchPoint &event)
+{
 	return event.lastPos();
 }
 
-inline auto touchPos(const QTouchEvent::TouchPoint &event) {
+inline auto touchPos(const QTouchEvent::TouchPoint &event)
+{
 	return event.pos();
 }
 
