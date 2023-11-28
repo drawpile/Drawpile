c3df22b0913aa422a6c723426086d1f6c5c8e90c
askmeaboutloom <askmeaboutlo0m@users.noreply.github.com>
2023-11-28 13:26:54 +0100

    Run OpenSSL Configure with perl on Windows
    Because the shebang line doesn't work there.

--- .github/scripts/cmake/BuildDependency.cmake
+++ .github/scripts/cmake/BuildDependency.cmake
@@ -188,13 +188,25 @@ function(_build_automake build_type target_bits source_dir)
 		set(install install)
 	endif()
 
-	execute_process(
-		COMMAND "${CMAKE_COMMAND}" -E env ${env}
-			"${configure}" ${prefix} ${configure_flags}
-		COMMAND_ECHO STDOUT
-		WORKING_DIRECTORY "${source_dir}"
-		COMMAND_ERROR_IS_FATAL ANY
-	)
+	# On Windows, the only usable thing here is OpenSSL's pseudo-automake Perl
+	# script. Shebangs don't work on Windows, so we have to run it explicitly.
+	if(WIN32)
+		execute_process(
+			COMMAND "${CMAKE_COMMAND}" -E env ${env}
+				perl "${configure}" ${prefix} ${configure_flags}
+			COMMAND_ECHO STDOUT
+			WORKING_DIRECTORY "${source_dir}"
+			COMMAND_ERROR_IS_FATAL ANY
+		)
+	else()
+		execute_process(
+			COMMAND "${CMAKE_COMMAND}" -E env ${env}
+				"${configure}" ${prefix} ${configure_flags}
+			COMMAND_ECHO STDOUT
+			WORKING_DIRECTORY "${source_dir}"
+			COMMAND_ERROR_IS_FATAL ANY
+		)
+	endif()
 
 	# More special stuff for OpenSSL which has a broken install target
 	if(ARG_BROKEN_INSTALL)
