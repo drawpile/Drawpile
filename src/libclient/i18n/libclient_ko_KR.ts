ba2e91479309502f8eff9f457c942e6c30dbcc53
askmeaboutloom <askmeaboutlo0m@users.noreply.github.com>
2023-12-08 15:19:47 +0100

    Integrate Korean translation
    Renaming the files to ko_KR instead of just ko because Weblate does that
    incorrectly, changing the language in the XML files accordingly and
    adding them to SUPPORTED_LANGS.

--- src/libclient/i18n/libclient_ko_KR.ts
+++ src/libclient/i18n/libclient_ko_KR.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="ko">
+<TS version="2.1" language="ko_KR">
 <context>
     <name>AvatarListModel</name>
     <message>
