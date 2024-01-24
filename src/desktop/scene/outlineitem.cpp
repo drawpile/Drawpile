8088dbec498947926893b023bc71427d31ee10ca
askmeaboutloom <askmeaboutlo0m@users.noreply.github.com>
2024-01-24 16:50:51 +0100

    Hide brush outline harder when it's not there
    Because apparently on some systems having it be not visible causes
    graphical corruption. Now the bounds are set to nothing and no painting
    is done when the outline is invisible.

--- src/desktop/scene/outlineitem.cpp
+++ src/desktop/scene/outlineitem.cpp
@@ -18,13 +18,10 @@ QRectF OutlineItem::boundingRect() const
 void OutlineItem::setOutline(qreal outlineSize, qreal outlineWidth)
 {
 	if(outlineSize != m_outlineSize || outlineWidth != m_outlineWidth) {
-		prepareGeometryChange();
 		m_outlineSize = outlineSize;
 		m_outlineWidth = outlineWidth;
 		qreal offset = m_outlineSize * -0.5;
 		m_bounds = QRectF(offset, offset, outlineSize, outlineSize);
-		m_outerBounds = m_bounds.marginsAdded(
-			QMarginsF(outlineWidth, outlineWidth, outlineWidth, outlineWidth));
 		updateVisibility();
 	}
 }
@@ -52,25 +49,35 @@ void OutlineItem::setOnCanvas(bool onCanvas)
 void OutlineItem::paint(
 	QPainter *painter, const QStyleOptionGraphicsItem *, QWidget *)
 {
-	painter->save();
-	QPen pen(QColor(96, 191, 96));
-	pen.setCosmetic(true);
-	pen.setWidthF(m_outlineWidth);
-	painter->setPen(pen);
-	painter->setCompositionMode(QPainter::RasterOp_SourceXorDestination);
-	if(m_square) {
-		painter->drawRect(m_bounds);
-	} else {
-		painter->drawEllipse(m_bounds);
+	if(isVisible()) {
+		painter->save();
+		QPen pen(QColor(96, 191, 96));
+		pen.setCosmetic(true);
+		pen.setWidthF(m_outlineWidth);
+		painter->setPen(pen);
+		painter->setCompositionMode(QPainter::RasterOp_SourceXorDestination);
+		if(m_square) {
+			painter->drawRect(m_bounds);
+		} else {
+			painter->drawEllipse(m_bounds);
+		}
+		painter->restore();
 	}
-	painter->restore();
 }
 
 void OutlineItem::updateVisibility()
 {
-	setVisible(
-		m_onCanvas && m_visibleInMode && m_outlineSize > 0.0 &&
-		m_outlineWidth > 0.0 && m_bounds.isValid());
+	bool visible = m_onCanvas && m_visibleInMode && m_outlineSize > 0.0 &&
+				   m_outlineWidth > 0.0 && m_bounds.isValid();
+	setVisible(visible);
+	QRectF outerBounds = visible ? m_bounds.marginsAdded(QMarginsF(
+									   m_outlineWidth, m_outlineWidth,
+									   m_outlineWidth, m_outlineWidth))
+								 : QRectF();
+	if(outerBounds != m_outerBounds) {
+		prepareGeometryChange();
+		m_outerBounds = outerBounds;
+	}
 }
 
 }
