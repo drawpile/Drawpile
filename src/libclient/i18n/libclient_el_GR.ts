0a10f6eb41dc2e4ed027e558702e176e4f497811
askmeaboutloom <askmeaboutlo0m@users.noreply.github.com>
2024-02-18 02:01:02 +0100

    Integrate translations for 2.2.1

--- src/libclient/i18n/libclient_el_GR.ts
+++ src/libclient/i18n/libclient_el_GR.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="el">
+<TS version="2.1" language="el_GR">
 <context>
     <name>AvatarListModel</name>
     <message>
