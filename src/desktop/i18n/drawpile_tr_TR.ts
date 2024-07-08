dd778b6dd49191ac7e1fd0505901601dc928263e
Weblate Translation Memory <noreply-mt-weblate-translation-memory@weblate.org>
2024-07-08 14:22:13 +0000

    Translated using Weblate (Turkish) [ci skip]
    Currently translated at 97.3% (1718 of 1764 strings)
    
    Translation: Drawpile/desktop
    Translate-URL: https://hosted.weblate.org/projects/drawpile/desktop/tr/

--- src/desktop/i18n/drawpile_tr_TR.ts
+++ src/desktop/i18n/drawpile_tr_TR.ts
@@ -8240,7 +8240,7 @@ Values above 0.5 may not be noticeable.</source>
     <message>
         <location line="+6"/>
         <source>The download page for Drawpile %1 could not be opened, please visit &lt;strong&gt;drawpile.net&lt;/strong&gt; manually.</source>
-        <translation type="unfinished">The download page for Drawpile %1 could not be opened, please visit &lt;strong&gt;drawpile.net&lt;/strong&gt; manually.</translation>
+        <translation type="unfinished">Drawpile %1 indirme sayfası açılamadı, lütfen &lt;em&gt;drawpile.net&lt;/em&gt; adresini manuel olarak ziyaret ediniz.</translation>
     </message>
 </context>
 <context>
