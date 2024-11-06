58de00ff1f0a6f4c164a15d197b08961087b12a8
askmeaboutloom <askmeaboutlo0m@users.noreply.github.com>
2024-11-06 16:24:21 +0100

    Also set file description and version for Windows
    Because otherwise SignPath doesn't pick up on the product name and
    version correctly.

--- .github/scripts/edit-pe-metadata.cmake
+++ .github/scripts/edit-pe-metadata.cmake
@@ -1,6 +1,6 @@
 # SPDX-License-Identifier: MIT
-if(NOT PRODUCT_NAME OR NOT PRODUCT_VERSION OR NOT SEARCH_PATHS)
-    message(FATAL_ERROR "PRODUCT_NAME, PRODUCT_VERSION and SEARCH_PATHS are required")
+if(NOT FILE_DESCRIPTION OR NOT FILE_VERSION OR NOT PRODUCT_NAME OR NOT PRODUCT_VERSION OR NOT SEARCH_PATHS)
+    message(FATAL_ERROR "FILE_DESCRIPTION, FILE_VERSION, PRODUCT_NAME, PRODUCT_VERSION and SEARCH_PATHS are required")
 endif()
 
 find_program(RCEDIT_COMMAND rcedit REQUIRED)
@@ -17,6 +17,8 @@ foreach(pe_path IN LISTS pe_paths)
         COMMAND
             ${RCEDIT_COMMAND}
             "${pe_path}"
+            --set-version-string FileDescription "${FILE_DESCRIPTION}"
+            --set-version-string FileVersion "${FILE_VERSION}"
             --set-version-string ProductName "${PRODUCT_NAME}"
             --set-version-string ProductVersion "${PRODUCT_VERSION}"
         COMMAND_ECHO STDOUT
