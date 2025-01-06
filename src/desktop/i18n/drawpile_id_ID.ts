5eaf69b70eb0fc3ee1b94f3616c22a940a106a6e
LibreTranslate <noreply-mt-libretranslate@weblate.org>
2025-01-06 13:52:06 +0000

    Translated using Weblate (Indonesian) [ci skip]
    Currently translated at 13.5% (281 of 2072 strings)
    
    Translation: Drawpile/desktop
    Translate-URL: https://hosted.weblate.org/projects/drawpile/desktop/id/

--- src/desktop/i18n/drawpile_id_ID.ts
+++ src/desktop/i18n/drawpile_id_ID.ts
@@ -1547,7 +1547,7 @@ Sederhanakan kanvas dan atur ulang secara manual sebelum ruang kehabisan.</trans
     <message>
         <location line="+1"/>
         <source>Enter operator password</source>
-        <translation type="unfinished"></translation>
+        <translation>Masukkan kata sandi operator</translation>
     </message>
     <message>
         <location line="+61"/>
@@ -1563,7 +1563,7 @@ Sederhanakan kanvas dan atur ulang secara manual sebelum ruang kehabisan.</trans
     <message>
         <location line="-2273"/>
         <source>Would you like to join the session instead?</source>
-        <translation type="unfinished"></translation>
+        <translation>Apakah Anda ingin bergabung dengan sesi bukan?</translation>
     </message>
     <message>
         <location line="+2"/>
