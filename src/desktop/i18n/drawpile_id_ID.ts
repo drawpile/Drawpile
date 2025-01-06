78170c21dc42a7771bbf1a41613abff99ef01932
Rayytra <rayytra@gmail.com>
2025-01-06 14:14:42 +0000

    Translated using Weblate (Indonesian) [ci skip]
    Currently translated at 15.7% (327 of 2072 strings)
    
    Translation: Drawpile/desktop
    Translate-URL: https://hosted.weblate.org/projects/drawpile/desktop/id/

--- src/desktop/i18n/drawpile_id_ID.ts
+++ src/desktop/i18n/drawpile_id_ID.ts
@@ -1866,7 +1866,7 @@ Are you sure youwant to start recording debug dumps?</source>
     <message>
         <location line="+1"/>
         <source>Paste in View Center</source>
-        <translation>Tempel di View Center</translation>
+        <translation>Tempel di Tengah Penglihatan</translation>
     </message>
     <message>
         <location line="+2"/>
