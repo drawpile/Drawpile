842f8348437a9a74171f7123683e1f867ccc13a4
askmeaboutloom <askmeaboutlo0m@users.noreply.github.com>
2025-03-17 13:00:12 +0100

    Properly linkify URLs with an @ in the path
    It would take them as part of the query string and then stop after the
    next slash.

--- src/libclient/tests/html.cpp
+++ src/libclient/tests/html.cpp
@@ -17,6 +17,7 @@ private slots:
 		QTest::newRow("just domain") << "www.example.com" << "<a href=\"http://www.example.com\">www.example.com</a>";
 		QTest::newRow("text w/domain") << "hello www.example.com world" << "hello <a href=\"http://www.example.com\">www.example.com</a> world";
 		QTest::newRow("full URL") << "hello https://www.example.com/ world" << "hello <a href=\"https://www.example.com/\">https://www.example.com/</a> world";
+		QTest::newRow("full URL with an @ in the path") << "hello https://www.example.com/@someone/12345 world" << "hello <a href=\"https://www.example.com/@someone/12345\">https://www.example.com/@someone/12345</a> world";
 	}
 
 	void testLinkify()
