8bff2f755bb950ec33c284b38c211da957b871bc
Weblate Translation Memory <noreply-mt-weblate-translation-memory@weblate.org>
2025-03-19 06:45:13 +0100

    Translated using Weblate (Portuguese (Portugal)) [ci skip]
    Currently translated at 79.4% (286 of 360 strings)
    
    Translation: Drawpile/libclient
    Translate-URL: https://hosted.weblate.org/projects/drawpile/libclient/pt_PT/

--- src/libclient/i18n/libclient_pt_PT.ts
+++ src/libclient/i18n/libclient_pt_PT.ts
@@ -1570,7 +1570,7 @@ Select a regular layer instead.</source>
     <message>
         <location line="+558"/>
         <source>Unknown version %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Versão desconhecida</translation>
     </message>
     <message>
         <location line="+280"/>
