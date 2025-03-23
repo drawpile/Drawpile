fd2c4b3114de83457dc710dca9b711eec1ac70fc
Weblate Translation Memory <noreply-mt-weblate-translation-memory@weblate.org>
2025-03-23 10:27:51 +0100

    Translated using Weblate (French) [ci skip]
    Currently translated at 45.3% (1027 of 2265 strings)
    
    Translation: Drawpile/desktop
    Translate-URL: https://hosted.weblate.org/projects/drawpile/desktop/fr/

--- src/desktop/i18n/drawpile_fr_FR.ts
+++ src/desktop/i18n/drawpile_fr_FR.ts
@@ -7957,7 +7957,7 @@ Les valeurs supérieures à 0,5 peuvent ne pas être visibles.</translation>
     <message>
         <location filename="../dialogs/videoexportdialog.cpp" line="+31"/>
         <source>Image Series</source>
-        <translation type="unfinished"></translation>
+        <translation>Série d&apos;images</translation>
     </message>
     <message>
         <location line="+2"/>
