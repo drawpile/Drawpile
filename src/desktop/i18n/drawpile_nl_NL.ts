502d3e5e46a175b91169ea06bffa52f47df7e827
askmeaboutloom <askmeaboutlo0m@users.noreply.github.com>
2025-03-26 04:37:32 +0100

    Move Dutch translation from nl to nl_NL
    To match the format of all other translations.

--- src/desktop/i18n/drawpile_nl_NL.ts
+++ src/desktop/i18n/drawpile_nl_NL.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="nl">
+<TS version="2.1" language="nl_NL">
 <context>
     <name>AbstractWidgetList</name>
     <message>
