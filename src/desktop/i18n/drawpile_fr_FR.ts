9d73ed84accb4323b123510cae58d6a3d69fb930
Weblate <noreply-mt-weblate@weblate.org>
2025-03-27 09:55:26 +0100

    Translated using Weblate (French) [ci skip]
    Currently translated at 52.5% (1190 of 2265 strings)
    
    Translation: Drawpile/desktop
    Translate-URL: https://hosted.weblate.org/projects/drawpile/desktop/fr/

--- src/desktop/i18n/drawpile_fr_FR.ts
+++ src/desktop/i18n/drawpile_fr_FR.ts
@@ -8602,7 +8602,7 @@ Les valeurs supérieures à 0,5 peuvent ne pas être visibles.</translation>
     <message>
         <location line="+2"/>
         <source>Enter password:</source>
-        <translation type="unfinished"></translation>
+        <translation>Entrez le mot de passe&#xa0;:</translation>
     </message>
     <message>
         <location line="+12"/>
