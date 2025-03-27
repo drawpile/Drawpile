2ebd1165df1f2d3a4052f743068077e2c8d03395
Weblate <noreply-mt-weblate@weblate.org>
2025-03-27 09:51:28 +0100

    Translated using Weblate (French) [ci skip]
    Currently translated at 52.3% (1185 of 2265 strings)
    
    Translation: Drawpile/desktop
    Translate-URL: https://hosted.weblate.org/projects/drawpile/desktop/fr/

--- src/desktop/i18n/drawpile_fr_FR.ts
+++ src/desktop/i18n/drawpile_fr_FR.ts
@@ -8516,7 +8516,7 @@ Les valeurs supérieures à 0,5 peuvent ne pas être visibles.</translation>
     <message>
         <location line="+1"/>
         <source>Unrestricted</source>
-        <translation type="unfinished"></translation>
+        <translation>Non restreinte</translation>
     </message>
     <message>
         <location line="+1"/>
