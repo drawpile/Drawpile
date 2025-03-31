1cb95a185a1fbbdbcc78008d577685bf7b18491b
Weblate Translation Memory <noreply-mt-weblate-translation-memory@weblate.org>
2025-03-31 13:21:46 +0200

    Translated using Weblate (French) [ci skip]
    Currently translated at 60.4% (1369 of 2265 strings)
    
    Translation: Drawpile/desktop
    Translate-URL: https://hosted.weblate.org/projects/drawpile/desktop/fr/

--- src/desktop/i18n/drawpile_fr_FR.ts
+++ src/desktop/i18n/drawpile_fr_FR.ts
@@ -9845,7 +9845,7 @@ Les valeurs supérieures à 0,5 peuvent ne pas être visibles.</translation>
     <message>
         <location line="+4"/>
         <source>Roles</source>
-        <translation type="unfinished"></translation>
+        <translation>Rôles</translation>
     </message>
     <message>
         <location line="+8"/>
