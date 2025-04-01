2ee047c8655e690376d61183bb61b2f74c53652b
Weblate Translation Memory <noreply-mt-weblate-translation-memory@weblate.org>
2025-04-01 15:58:24 +0200

    Translated using Weblate (French) [ci skip]
    Currently translated at 62.8% (1423 of 2265 strings)
    
    Translation: Drawpile/desktop
    Translate-URL: https://hosted.weblate.org/projects/drawpile/desktop/fr/

--- src/desktop/i18n/drawpile_fr_FR.ts
+++ src/desktop/i18n/drawpile_fr_FR.ts
@@ -10735,7 +10735,7 @@ Les valeurs supérieures à 0,5 peuvent ne pas être visibles.</translation>
     <message>
         <location line="+19"/>
         <source>RGB</source>
-        <translation type="unfinished"></translation>
+        <translation>RGB</translation>
     </message>
     <message>
         <location line="+203"/>
