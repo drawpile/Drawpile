c69f8de7b29d09c01f15d50e2484e80e2709dce5
Weblate <noreply-mt-weblate@weblate.org>
2025-04-08 11:31:26 +0200

    Translated using Weblate (French) [ci skip]
    Currently translated at 63.7% (1443 of 2265 strings)
    
    Translation: Drawpile/desktop
    Translate-URL: https://hosted.weblate.org/projects/drawpile/desktop/fr/

--- src/desktop/i18n/drawpile_fr_FR.ts
+++ src/desktop/i18n/drawpile_fr_FR.ts
@@ -10152,7 +10152,7 @@ Les valeurs supérieures à 0,5 peuvent ne pas être visibles.</translation>
     <message>
         <location filename="../dialogs/startdialog/host/dialogs.cpp" line="+282"/>
         <source>Load Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Charger les Paramètres</translation>
     </message>
     <message>
         <location line="+14"/>
