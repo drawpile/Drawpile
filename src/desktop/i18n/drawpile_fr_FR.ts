e468ea733efc296894b794d2b347bd3d67568b7d
Weblate <noreply-mt-weblate@weblate.org>
2025-04-24 10:59:53 +0200

    Translated using Weblate (French) [ci skip]
    Currently translated at 68.7% (1558 of 2265 strings)
    
    Translation: Drawpile/desktop
    Translate-URL: https://hosted.weblate.org/projects/drawpile/desktop/fr/

--- src/desktop/i18n/drawpile_fr_FR.ts
+++ src/desktop/i18n/drawpile_fr_FR.ts
@@ -11689,7 +11689,7 @@ Les valeurs supérieures à 0,5 peuvent ne pas être visibles.</translation>
     <message>
         <location line="+20"/>
         <source>Constrain</source>
-        <translation type="unfinished"></translation>
+        <translation>Contrainte</translation>
     </message>
     <message>
         <location line="+12"/>
