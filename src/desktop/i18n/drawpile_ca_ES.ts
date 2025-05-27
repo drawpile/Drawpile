25821d4587697733132701d08cb9352cac680031
Weblate Translation Memory <noreply-mt-weblate-translation-memory@weblate.org>
2025-05-27 11:19:02 +0200

    Translated using Weblate (Catalan) [ci skip]
    Currently translated at 63.1% (1430 of 2265 strings)
    
    Translation: Drawpile/desktop
    Translate-URL: https://hosted.weblate.org/projects/drawpile/desktop/ca/

--- src/desktop/i18n/drawpile_ca_ES.ts
+++ src/desktop/i18n/drawpile_ca_ES.ts
@@ -5735,7 +5735,7 @@ Take note of &quot;Dabs per basic radius&quot; and &quot;Dabs per actual radius&
         <source>Eraser</source>
         <comment>mypaintsetting</comment>
         <extracomment>This text comes from the MyPaint brush engine. You can skip translating it if it&apos;s too difficult.</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Esborrador</translation>
     </message>
     <message>
         <location line="+3"/>
