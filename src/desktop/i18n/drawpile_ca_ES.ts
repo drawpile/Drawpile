0445c8521878d2e5e6303db342c0663551f1db9b
Weblate Translation Memory <noreply-mt-weblate-translation-memory@weblate.org>
2025-05-28 10:18:10 +0200

    Translated using Weblate (Catalan) [ci skip]
    Currently translated at 68.5% (1552 of 2265 strings)
    
    Translation: Drawpile/desktop
    Translate-URL: https://hosted.weblate.org/projects/drawpile/desktop/ca/

--- src/desktop/i18n/drawpile_ca_ES.ts
+++ src/desktop/i18n/drawpile_ca_ES.ts
@@ -12496,7 +12496,7 @@ or via Animation ▸ New Track.</source>
     <message>
         <location line="+1"/>
         <source>&amp;Mute</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Silencia</translation>
     </message>
     <message>
         <location line="+4"/>
