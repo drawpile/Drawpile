b92b253d39b2f68087f90c15b740d84051c9f78b
Weblate <noreply-mt-weblate@weblate.org>
2025-05-28 09:47:31 +0200

    Translated using Weblate (Catalan) [ci skip]
    Currently translated at 64.6% (1464 of 2265 strings)
    
    Translation: Drawpile/desktop
    Translate-URL: https://hosted.weblate.org/projects/drawpile/desktop/ca/

--- src/desktop/i18n/drawpile_ca_ES.ts
+++ src/desktop/i18n/drawpile_ca_ES.ts
@@ -10941,7 +10941,7 @@ Values above 0.5 may not be noticeable.</source>
     <message>
         <location line="+1366"/>
         <source>Sketch: </source>
-        <translation type="unfinished"></translation>
+        <translation>Esbós </translation>
     </message>
     <message>
         <location line="+165"/>
