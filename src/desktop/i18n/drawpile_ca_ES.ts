be6b7216afeb6c466697f4a4e7f7df4225193725
Weblate Translation Memory <noreply-mt-weblate-translation-memory@weblate.org>
2025-05-29 14:39:47 +0200

    Translated using Weblate (Catalan) [ci skip]
    Currently translated at 70.5% (1598 of 2265 strings)
    
    Translation: Drawpile/desktop
    Translate-URL: https://hosted.weblate.org/projects/drawpile/desktop/ca/

--- src/desktop/i18n/drawpile_ca_ES.ts
+++ src/desktop/i18n/drawpile_ca_ES.ts
@@ -7574,7 +7574,7 @@ Values above 0.5 may not be noticeable.</source>
     <message>
         <location line="+2"/>
         <source>Servers</source>
-        <translation type="unfinished"></translation>
+        <translation>Seguidors</translation>
     </message>
     <message>
         <location line="+3"/>
