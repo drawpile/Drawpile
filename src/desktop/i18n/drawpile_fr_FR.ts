002838a49334229d0093b008e95d0d187ead3cc7
Weblate <noreply-mt-weblate@weblate.org>
2025-06-23 16:56:46 +0200

    Translated using Weblate (French) [ci skip]
    Currently translated at 83.3% (1889 of 2265 strings)
    
    Translation: Drawpile/desktop
    Translate-URL: https://hosted.weblate.org/projects/drawpile/desktop/fr/

--- src/desktop/i18n/drawpile_fr_FR.ts
+++ src/desktop/i18n/drawpile_fr_FR.ts
@@ -11180,7 +11180,7 @@ Les valeurs supérieures à 0,5 peuvent ne pas être visibles.</translation>
     <message>
         <location line="+6"/>
         <source>Zoom: </source>
-        <translation type="unfinished"></translation>
+        <translation>Zoom&#xa0;: </translation>
     </message>
     <message>
         <location line="+1"/>
