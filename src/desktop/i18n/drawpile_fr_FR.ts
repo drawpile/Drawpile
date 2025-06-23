94cc626ad46c0a0564980da9476cfc3fd7bbc508
Weblate <noreply-mt-weblate@weblate.org>
2025-06-23 17:10:56 +0200

    Translated using Weblate (French) [ci skip]
    Currently translated at 84.3% (1911 of 2265 strings)
    
    Translation: Drawpile/desktop
    Translate-URL: https://hosted.weblate.org/projects/drawpile/desktop/fr/

--- src/desktop/i18n/drawpile_fr_FR.ts
+++ src/desktop/i18n/drawpile_fr_FR.ts
@@ -11906,7 +11906,7 @@ Les valeurs supérieures à 0,5 peuvent ne pas être visibles.</translation>
     <message>
         <location line="+3"/>
         <source>Save As…</source>
-        <translation type="unfinished"></translation>
+        <translation>Enregistrer sous…</translation>
     </message>
     <message>
         <location line="+375"/>
