d1eb5d25bfe2e51a36c1316b059d5ae1f873d384
Максим Горпиніч <gorpinicmaksim5@gmail.com>
2025-07-01 18:24:27 +0200

    Translated using Weblate (Ukrainian) [ci skip]
    Currently translated at 100.0% (424 of 424 strings)
    
    Translation: Drawpile/libclient
    Translate-URL: https://hosted.weblate.org/projects/drawpile/libclient/uk/

--- src/libclient/i18n/libclient_uk_UA.ts
+++ src/libclient/i18n/libclient_uk_UA.ts
@@ -74,7 +74,7 @@
     <message>
         <location line="+1"/>
         <source>Failed to start Drawpile: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Не вдалося розпочати розіграш: %1</translation>
     </message>
     <message>
         <location line="+1"/>
