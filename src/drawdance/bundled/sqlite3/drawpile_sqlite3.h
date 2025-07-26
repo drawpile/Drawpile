9bdc2f365e707ad33991b83c1fea5a2f99700693
askmeaboutloom <askmeaboutlo0m@users.noreply.github.com>
2025-07-26 22:38:37 +0200

    Make sure MSVC includes the bundled sqlite3.h
    It gets distracted by system headers sometimes and if they're outdated
    and are missing constants it breaks compilation. So now we include a
    header with a Drawpile-specific name that in turn includes the correct
    header from the current directory.

---
+++ src/drawdance/bundled/sqlite3/drawpile_sqlite3.h
@@ -0,0 +1,3 @@
+// MSVC includes this header from who-knows-where sometimes, so we have this
+// extra file to get the crayon out of its nose and pick the correct one.
+#include "sqlite3.h"
--- src/drawdance/libdb/dpdb/sql.h
+++ src/drawdance/libdb/dpdb/sql.h
@@ -2,7 +2,9 @@
 #ifndef DPDB_SQL_H
 #define DPDB_SQL_H
 #include <dpcommon/common.h>
-#include <sqlite3.h>
+// This header just includes the bundled sqlite3.h, it's just here because MSVC
+// sometimes gets distracted by some other, outdated sqlite3.h on the system.
+#include <drawpile_sqlite3.h>
 
 
 int DP_sql_init(void);
