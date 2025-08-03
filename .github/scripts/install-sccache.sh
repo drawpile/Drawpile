0a96cefdd84a82df8de0a47573aa1d952dfdd755
askmeaboutloom <askmeaboutlo0m@users.noreply.github.com>
2025-08-03 10:41:32 +0200

    Log sccache installation better
    To be able to tell what it's installing and such.

--- .github/scripts/install-sccache.sh
+++ .github/scripts/install-sccache.sh
@@ -18,10 +18,15 @@ if [[ $# < 2 ]]; then
 	exit_usage "Not enough arguments"
 fi
 
+echo "path: '$1'"
+echo "version: '$2'"
+echo "triplet: '$3'"
+
+url="https://github.com/mozilla/sccache/releases/download/v$2/sccache-v$2-$3.tar.gz"
+echo "url: '$url'"
+
 mkdir -p "$1/bin"
 cd "$1/bin"
 # Use gunzip because some BSD tar does not support -z whereas GNU tar requires
 # it but both support gunzip
-curl --retry 3 -L \
-	"https://github.com/mozilla/sccache/releases/download/v$2/sccache-v$2-$3.tar.gz" \
-	| gunzip | tar --strip-components 1 -xf -
+curl --retry 3 -L "$url" | gunzip | tar --strip-components 1 -xf -
