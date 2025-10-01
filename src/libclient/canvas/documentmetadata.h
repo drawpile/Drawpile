6c952614777824e12831bc5d43fcf0261a16e19a
askmeaboutloom <askmeaboutlo0m@users.noreply.github.com>
2025-10-01 16:54:54 +0200

    Format and clean up documentmetadata.{cpp,h}

--- src/libclient/canvas/documentmetadata.h
+++ src/libclient/canvas/documentmetadata.h
@@ -1,20 +1,17 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
-
-#ifndef DOCUMENTMETADATA_H
-#define DOCUMENTMETADATA_H
-
+#ifndef LIBCLIENT_CANVAS_DOCUMENTMETADATA_H
+#define LIBCLIENT_CANVAS_DOCUMENTMETADATA_H
 #include <QObject>
 
 namespace drawdance {
-   class DocumentMetadata;
+class DocumentMetadata;
 }
 
 namespace canvas {
 
 class PaintEngine;
 
-class DocumentMetadata final : public QObject
-{
+class DocumentMetadata final : public QObject {
 	Q_OBJECT
 public:
 	explicit DocumentMetadata(PaintEngine *engine, QObject *parent = nullptr);
@@ -25,16 +22,17 @@ public:
 signals:
 	void framerateChanged(double fps);
 	void frameCountChanged(int frameCount);
+	void frameRangeChanged(int first, int last);
 
 private slots:
 	void refreshMetadata(const drawdance::DocumentMetadata &dm);
 
 private:
 	PaintEngine *m_engine;
-	double m_framerate;
-	int m_frameCount;
+	double m_framerate = 0.0;
+	int m_frameCount = 0;
 };
 
 }
 
-#endif // DOCUMENTMETADATA_H
+#endif
