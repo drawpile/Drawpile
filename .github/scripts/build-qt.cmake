b517f5f0f1ccefc51f2e1f5e9b96a750d785cc17
askmeaboutloom <askmeaboutlo0m@users.noreply.github.com>
2026-02-28 06:38:17 +0100

    Handle more stylus buttons on Android
    Translating forward, backward and tertiary to separate mouse buttons,
    since otherwise they overlap and on some tablets you end up with two
    middle clicks.

--- .github/scripts/build-qt.cmake
+++ .github/scripts/build-qt.cmake
@@ -368,6 +368,7 @@ if(KRITA_QT)
 				qtbase:patches/android-wacom-qt5-krita.diff
 				qtbase:patches/android-xiaomi-qt5-krita.diff
 				qtbase:patches/android-screen-density-adjustment-qt5-krita.diff
+				qtbase:patches/android-mousebuttons-qt5-krita.diff
 				TARGET_BITS=64@qtbase:patches/android-16k-alignment-qt5-krita.diff
 	)
 else()
