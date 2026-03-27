598f558fafc44edddc28c62f858d624b212ad2dc
Weblate <noreply-mt-weblate@weblate.org>
2026-03-27 21:45:54 +0100

    Translated using Weblate (Czech) [ci skip]
    Currently translated at 2.0% (9 of 432 strings)
    
    Translation: Drawpile/libclient
    Translate-URL: https://hosted.weblate.org/projects/drawpile/libclient/cs/

--- src/libclient/i18n/libclient_cs_CZ.ts
+++ src/libclient/i18n/libclient_cs_CZ.ts
@@ -79,7 +79,7 @@
     <message>
         <location line="+1"/>
         <source>Setting up…</source>
-        <translation type="unfinished"></translation>
+        <translation>Nastavení</translation>
     </message>
     <message>
         <location line="+1"/>
