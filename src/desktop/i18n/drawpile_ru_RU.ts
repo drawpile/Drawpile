ca5951a308ce001fa2cfac74f0f11c38bf2f3553
Weblate Translation Memory <noreply-mt-weblate-translation-memory@weblate.org>
2026-09-26 16:33:50 +0200

    Translated using Weblate (Russian) [ci skip]
    Currently translated at 82.5% (2547 of 3085 strings)
    
    Translation: Drawpile/desktop
    Translate-URL: https://hosted.weblate.org/projects/drawpile/desktop/ru/

--- src/desktop/i18n/drawpile_ru_RU.ts
+++ src/desktop/i18n/drawpile_ru_RU.ts
@@ -2274,10 +2274,10 @@ To avoid this extra step in the future, use the Browse page or a direct link to 
         <location line="+4"/>
         <location filename="../widgets/timelinewidget.cpp" line="+6"/>
         <source>Copy Key Frame(s)</source>
-        <translation type="unfinished">
+        <translation>
             <numerusform></numerusform>
-            <numerusform></numerusform>
-            <numerusform></numerusform>
+            <numerusform>Скопировать ключевой кадр</numerusform>
+            <numerusform>Скопировать ключевой кадр</numerusform>
         </translation>
     </message>
     <message numerus="yes">
